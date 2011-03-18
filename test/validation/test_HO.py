8a6d51ca1d60d8249ac79b20acefdc6199042f86
Giovanni Petrucciani <sha1-8e354cf6922a028ac31e5a5c5a5d72f318c92145@cern.ch>
2011-03-18 16:46:04 +0000

    Validation suite, running but not validated

---
+++ test/validation/test_HO.py
@@ -0,0 +1,26 @@
+from TestClasses import *
+
+suite = []
+M='Hybrid'
+
+### Test the p-values 
+for R in [ 'CLs', 'CLsplusb' ]:
+    suite += [ (M, '*', MultiOptionTest("Counting_pValues_%s" % R, "simple-counting/counting-B5p5-Obs6-Syst30B.txt", M,
+                            "--singlePoint 5 --fork 4 -T 200 --clsAcc=1 --rule=%s" % R,
+                            {'LEP':'--testStat=LEP', 'TEV':'--testStat=TEV'})) ]
+
+for X in [ "LEP", "TEV" ]:
+        suite += [ (M, 'fast', SingleDatacardTest("HWW_pValues_%s"%X, "hww4ch-1fb-B-mH140.txt", M, 
+                        "--singlePoint 2 --fork 2 -T 100 --clsAcc=1 --testStat=%s"%X )) ]
+        suite += [ (M, 'full', SingleDatacardTest("HWW_pValues_%s"%X, "hww4ch-1fb-B-mH140.txt", M, 
+                        "--singlePoint 2 --fork 6 -T 250 --clsAcc=1 --testStat=%s"%X )) ]
+
+### Test the limits
+options="--fork 4 -T 50 -H ProfileLikelihood"
+suite += [ (M, 'fast', SingleDatacardTest("Counting", "simple-counting/counting-B5p5-Obs6-Syst30B.txt", M, options)) ]
+suite += [ (M, 'fast', SingleDatacardTest("HWW",      "hww4ch-1fb-B-mH140.txt",                         M, options)) ]
+
+suite += [ (M, 'full', MultiDatacardTest("Counting",  datacardGlob("simple-counting/counting-B5p5-Obs[16]*-S*[Uy].txt"), M, options)) ]
+for d in datacardGlob("hww4ch-1fb-B*mH*.txt"):
+    n = re.sub("hww4ch-1fb-(.*mH..0).txt","\\1",d)
+    suite += [ (M, 'full',  SingleDatacardTest('HWW_'+n, d, M, options)) ]
