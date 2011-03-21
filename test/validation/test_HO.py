b888f8bf73b67e5feea556acbc726454f37461a3
Giovanni Petrucciani <sha1-8e354cf6922a028ac31e5a5c5a5d72f318c92145@cern.ch>
2011-03-21 11:26:19 +0000

    More reasonable number of toys Keep fork to a maximum of 2 in fast test suite (otherwise when running multiple jobs at a time you can kill a machine)

--- test/validation/test_HO.py
+++ test/validation/test_HO.py
@@ -6,10 +6,10 @@ M='Hybrid'
 ### Test the p-values 
 for R in [ 'CLs', 'CLsplusb' ]:
     suite += [ (M, 'fast', MultiOptionTest("Counting_pValues_%s" % R, "simple-counting/counting-B5p5-Obs6-Syst30B.txt", M,
-                            "--singlePoint 5 --fork 4 -T 500 --clsAcc=0.01 --rule=%s" % R,
+                            "--singlePoint 5 --fork 2 -T 200 --clsAcc=0.01 --rule=%s" % R,
                             {'LEP':'--testStat=LEP', 'TEV':'--testStat=TEV'})) ]
     suite += [ (M, 'full', MultiOptionTest("Counting_pValues_%s" % R, "simple-counting/counting-B5p5-Obs6-Syst30B.txt", M,
-                            "--singlePoint 5 --fork 4 -T 500 --clsAcc=0.002 --rule=%s" % R,
+                            "--singlePoint 5 --fork 2 -T 500 --clsAcc=0.002 --rule=%s" % R,
                             {'LEP':'--testStat=LEP', 'TEV':'--testStat=TEV'})) ]
 
 for X in [ "LEP", "TEV" ]:
@@ -19,7 +19,8 @@ for X in [ "LEP", "TEV" ]:
                         "--singlePoint 2 --fork 6 -T 250 --clsAcc=1 --testStat=%s"%X )) ]
 
 ### Test the limits
-options="--fork 4 -T 50 -H ProfileLikelihood"
+optionsFast="--fork 2 -H ProfileLikelihood"
+optionsFull="--fork 6 -H ProfileLikelihood --rAbsAcc=0.05 --rRelAcc=0.02"
 suite += [ (M, 'fast', SingleDatacardTest("Counting", "simple-counting/counting-B5p5-Obs6-Syst30B.txt", M, options)) ]
 suite += [ (M, 'fast', SingleDatacardTest("HWW",      "hww4ch-1fb-B-mH140.txt",                         M, options)) ]
 
