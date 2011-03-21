eee86d9f063e26cc52b2a2cd351ab1d7c82cd9d5
Giovanni Petrucciani <sha1-8e354cf6922a028ac31e5a5c5a5d72f318c92145@cern.ch>
2011-03-21 13:56:18 +0000

    Further updates of validation suite

--- test/validation/test_HO.py
+++ test/validation/test_HO.py
@@ -14,17 +14,17 @@ for R in [ 'CLs', 'CLsplusb' ]:
 
 for X in [ "LEP", "TEV" ]:
         suite += [ (M, 'fast', SingleDatacardTest("HWW_pValues_%s"%X, "hww4ch-1fb-B-mH140.txt", M, 
-                        "--singlePoint 2 --fork 2 -T 100 --clsAcc=1 --testStat=%s"%X )) ]
+                        "--singlePoint 1 --fork 2 --clsAcc=0.01  --testStat=%s"%X )) ]
         suite += [ (M, 'full', SingleDatacardTest("HWW_pValues_%s"%X, "hww4ch-1fb-B-mH140.txt", M, 
-                        "--singlePoint 2 --fork 6 -T 250 --clsAcc=1 --testStat=%s"%X )) ]
+                        "--singlePoint 1 --fork 6 --clsAcc=0.002 --testStat=%s"%X )) ]
 
 ### Test the limits
 optionsFast="--fork 2 -H ProfileLikelihood"
 optionsFull="--fork 6 -H ProfileLikelihood --rAbsAcc=0.05 --rRelAcc=0.02"
-suite += [ (M, 'fast', SingleDatacardTest("Counting", "simple-counting/counting-B5p5-Obs6-Syst30B.txt", M, options)) ]
-suite += [ (M, 'fast', SingleDatacardTest("HWW",      "hww4ch-1fb-B-mH140.txt",                         M, options)) ]
+suite += [ (M, 'fast', SingleDatacardTest("Counting", "simple-counting/counting-B5p5-Obs6-Syst30B.txt", M, optionsFast)) ]
+suite += [ (M, 'full', SingleDatacardTest("HWW",      "hww4ch-1fb-B-mH140.txt",                         M, optionsFast)) ]
 
-suite += [ (M, 'full', MultiDatacardTest("Counting",  datacardGlob("simple-counting/counting-B5p5-Obs[16]*-S*[Uy].txt"), M, options)) ]
+suite += [ (M, 'full', MultiDatacardTest("Counting",  datacardGlob("simple-counting/counting-B5p5-Obs[16]*-S*[Uy].txt"), M, optionsFast)) ]
 for d in datacardGlob("hww4ch-1fb-B*mH*.txt"):
     n = re.sub("hww4ch-1fb-(.*mH..0).txt","\\1",d)
-    suite += [ (M, 'full',  SingleDatacardTest('HWW_'+n, d, M, options)) ]
+    suite += [ (M, 'full',  SingleDatacardTest('HWW_'+n, d, M, optionsFast)) ]
