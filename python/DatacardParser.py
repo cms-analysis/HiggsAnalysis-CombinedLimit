c1e651bf75dc258ab6f5b4a30f90cc519700e4cd
Roger Wolf <sha1-bf6c89fe049b9f9a193bed584ffb80166e4ef256@cern.ch>
2012-11-22 13:32:40 +0000

    allow for datacards w/o any signal for 0-jet event categories which are used for in situ background calibration

--- python/DatacardParser.py
+++ python/DatacardParser.py
@@ -114,7 +114,9 @@ def parseCard(file, options):
                 elif ret.isSignal[p] != s:
                     raise RuntimeError, "Process %s is declared as signal in some bin and as background in some other bin" % p
             ret.signals = [p for p,s in ret.isSignal.items() if s == True]
-            if len(ret.signals) == 0: raise RuntimeError, "You must have at least one signal process (id <= 0)"
+            ## HTT we want to allow this to be able to have categories that do not contain signal
+            ## HTT but that constrain backgrouns
+            #if len(ret.signals) == 0: raise RuntimeError, "You must have at least one signal process (id <= 0)"
         if f[0] == "rate":
             if processline == []: raise RuntimeError, "Missing line with process names before rate line" 
             if sigline == []:     raise RuntimeError, "Missing line with process id before rate line" 
@@ -176,7 +178,9 @@ def parseCard(file, options):
         ns_bin = sum([(ret.exp[b][p] != 0) for (b1,p,s) in ret.keyline if b1 == b and s == True])
         nb_bin = sum([(ret.exp[b][p] != 0) for (b1,p,s) in ret.keyline if b1 == b and s != True])
         if np_bin == 0: raise RuntimeError, "Bin %s has no processes contributing to it" % b
-        if ns_bin == 0: raise RuntimeError, "Bin %s has no signal processes contributing to it" % b
+        ## HTT we want to allow for datacards w/o signal to allow for categories that do not contain
+        ## HTT signal but constrain backgrounds
+        #if ns_bin == 0: raise RuntimeError, "Bin %s has no signal processes contributing to it" % b
         if nb_bin == 0: raise RuntimeError, "Bin %s has no background processes contributing to it" % b
     # cleanup systematics that have no effect to avoid zero derivatives
     syst2 = []
