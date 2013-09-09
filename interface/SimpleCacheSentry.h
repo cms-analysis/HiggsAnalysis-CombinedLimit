acfbbe5ee42716f1cf3d4c8c3f2d422913ece8ce
Jake <andersj@fnal.gov>
2013-09-09 10:48:38 -0500

    Revert "implemented needed function in SimplCacheSentry"
    This reverts commit 98d3db5a93ecffe1a240856751910fc648e97f63.
    
    this will revert to commit fix to RooErfExpPdf without the changes to SimpleCacheSentry.

--- interface/SimpleCacheSentry.h
+++ interface/SimpleCacheSentry.h
@@ -32,7 +32,6 @@ class SimpleCacheSentry : public RooAbsArg {
         virtual void attachToVStore(RooVectorDataStore& vstore) {}
         virtual void setTreeBranchStatus(TTree& t, Bool_t active) {}
         virtual void fillTreeBranch(TTree& t) {}
-	virtual Bool_t isIdentical(const RooAbsArg& other, Bool_t assumeSameType=kFALSE) ;
     private:
         RooSetProxy _deps;
         ClassDef(SimpleCacheSentry,1) 
