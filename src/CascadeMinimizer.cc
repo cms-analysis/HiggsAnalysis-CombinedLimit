bb6e5af0183823315f990df6d484bf277e4b0fa6
nick <n.c.k.wardle@googlemail.com>
2013-09-20 12:21:04 +0200

    Revert "fixes to calculate limits with workspaces containing multipdfs"
    This reverts commit 221c7314c5d11239eba605459db2268a987042df.

--- src/CascadeMinimizer.cc
+++ src/CascadeMinimizer.cc
@@ -296,14 +296,9 @@ void CascadeMinimizer::multipleMinimize(const RooArgSet &reallyCleanParameters, 
 	     }
 	
       if (!isValidCombo && runShortCombinations) continue;
-      
-      if (verbose>2) {
-	std::cout << "Setting indices := ";
-	for (int id=0;id<numIndeces;id++) {
-		std::cout << ((RooCategory*)(pdfCategoryIndeces.at(id)))->getIndex() << " ";
-	}
-        std::cout << std::endl;
-      }
+//    for (int id=0;id<backgroundPdfCategory->numTypes();id++){
+
+      std::cout << std::endl;
 
       if (fitCounter>0) params->assignValueOnly(reallyCleanParameters); // no need to reset from 0'th fit
 
