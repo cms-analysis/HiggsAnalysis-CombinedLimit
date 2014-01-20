31056390a79106cc73bc516817d0a1660b950117
Giovanni <gpetruc@gmail.com>
2014-01-20 14:11:00 +0100

    vectorized MultiPdf, nexted AddPdfs, RooExponential and RooPower

---
+++ interface/CachingMultiPdf.h
@@ -0,0 +1,39 @@
+#ifndef CachingMultiPdf_h
+#define CachingMultiPdf_h
+
+#include "../interface/RooMultiPdf.h"
+#include "../interface/CachingNLL.h"
+#include <RooAbsData.h>
+#include <RooAddPdf.h>
+#include <vector>
+
+namespace cacheutils {
+    class CachingMultiPdf : public CachingPdfBase {
+        public:
+            CachingMultiPdf(const RooMultiPdf &pdf, const RooArgSet &obs) ;
+            ~CachingMultiPdf() ;
+            virtual const std::vector<Double_t> & eval(const RooAbsData &data) ;
+            const RooAbsReal *pdf() const { return pdf_; }
+            virtual void  setDataDirty() ;
+        protected:
+            const RooMultiPdf * pdf_;
+            boost::ptr_vector<CachingPdfBase>  cachingPdfs_;
+    };
+
+    class CachingAddPdf : public CachingPdfBase {
+        public:
+            CachingAddPdf(const RooAddPdf &pdf, const RooArgSet &obs) ;
+            ~CachingAddPdf() ;
+            virtual const std::vector<Double_t> & eval(const RooAbsData &data) ;
+            const RooAbsReal *pdf() const { return pdf_; }
+            virtual void  setDataDirty() ;
+        protected:
+            const RooAddPdf * pdf_;
+            std::vector<const RooAbsReal *> coeffs_;
+            boost::ptr_vector<CachingPdfBase>  cachingPdfs_;
+            std::vector<Double_t> work_;
+    };
+
+} // namespace
+
+#endif
---
+++ interface/VectorizedSimplePdfs.h
@@ -0,0 +1,31 @@
+#ifndef VectorizedExponential_h
+#define VectorizedExponential_h
+
+#include <RooExponential.h>
+#include <RooAbsData.h>
+#include "../interface/HGGRooPdfs.h"
+#include <vector>
+
+class VectorizedExponential {
+    public:
+        VectorizedExponential(const RooExponential &pdf, const RooAbsData &data) ;
+        void fill(std::vector<Double_t> &out) const ;
+    private:
+        const RooRealVar * x_;
+        const RooAbsReal * lambda_;
+        std::vector<Double_t> xvals_;
+        mutable std::vector<Double_t> work_;
+};
+
+class VectorizedPower {
+    public:
+        VectorizedPower(const RooPower &pdf, const RooAbsData &data) ;
+        void fill(std::vector<Double_t> &out) const ;
+    private:
+        const RooRealVar * x_;
+        const RooAbsReal * exponent_;
+        std::vector<Double_t> xvals_;
+        mutable std::vector<Double_t> work_;
+};
+
+#endif
