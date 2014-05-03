68335c93070c4f2e1fde5e60c50146e62a712451
usarica <ulascan90@gmail.com>
2014-05-03 18:24:15 +0200

    Cumulative update of options

--- python/SpinZeroStructure.py
+++ python/SpinZeroStructure.py
@@ -6,14 +6,26 @@ from HiggsAnalysis.CombinedLimit.PhysicsModel import *
 class SpinZeroHiggs(PhysicsModel):
     def __init__(self):
         self.mHRange = []
+
+        self.muFloating = True
         self.muAsPOI = False
-        self.muFloating = False
-        self.fg4fixed = False
-        self.phiPOI = False
-        self.phiFloating = False
+
+        self.fai1Floating = True
+        self.fai1POI = True
+
+        self.phiai1Floating = False
+        self.phiai1POI = False
+
+        self.fai2Floating = False
+        self.fai2POI = False
+
+        self.phiai2Floating = False
+        self.phiai2POI = False
+
         self.poiMap = []
         self.pois = {}
         self.verbose = False
+
     def setModelBuilder(self, modelBuilder):
         PhysicsModel.setModelBuilder(self,modelBuilder)
         self.modelBuilder.doModelBOnly = False
@@ -36,62 +48,113 @@ class SpinZeroHiggs(PhysicsModel):
 
     def setPhysicsOptions(self,physOptions):
         for po in physOptions:
-            #if po == "muAsPOI":
-            if 'muAsPOI' in po: 
+            if 'muFixed' in po: 
+                print "Will consider the signal strength as a fixed parameter"
+                self.muFloating = False
+            elif 'muAsPOI' in po: 
                 print "Will consider the signal strength as a parameter of interest"
                 self.muAsPOI = True
+
+            if 'fai1Fixed' in po or 'fai1NotPOI' in po: 
+                print "CMS_zz4l_fai1 is NOT A POI"
+                self.fai1POI = False
+                if 'fai1Fixed' in po:
+                    print "Will fix CMS_zz4l_fai1 to 0"
+                    self.fai1Floating = False
+                
+            if 'phiai1Floating' in po or 'phiai1AsPOI' in po: 
+                print "Will consider phase ai1 as a floating parameter"
+                self.phiai1Floating = True
+                if 'phiai1AsPOI' in po: 
+                    print "Will consider phase ai1 as a parameter of interest"
+                    self.phiai1POI = True
+
+            if 'fai2Floating' in po or 'fai2AsPOI' in po: 
+                print "Will float CMS_zz4l_fai2"
+                self.fai2Floating = True
+                if 'fai2AsPOI' in po:
+                    self.fai2POI = True
+
+            if 'phiai2Floating' in po or 'phiai2AsPOI' in po: 
+                print "Will consider phase ai2 as a floating parameter"
+                self.phiai2Floating = True
+                if 'phiai2AsPOI' in po: 
+                    print "Will consider phase ai2 as a parameter of interest"
+                    self.phiai2POI = True
+
+            if not self.muAsPOI and not self.fai1POI and not self.fai2POI and not self.phiai1POI and not self.phiai2POI:
+                print "No POIs detected: Switching to default configuration: Floating nuisance mu, floating POI fai1, eveything else fixed"
                 self.muFloating = True
-            #if po == "muFloating":
-            if 'muFloating' in po: 
-                print "Will consider the signal strength as a floating parameter (as a paraeter of interest if --PO muAsPOI is specified, as a nuisance otherwise)"
-                self.muFloating = True
-            #if po == "fg4fixed":
-            if 'fg4fixed' in po: 
-                print "Will fix CMS_zz4l_fg4 to 0 and make signal strength as a parameter of interest"
-                self.muAsPOI = True
-                self.muFloating = True
-                self.fg4fixed = True
-            #if po == "muFloating,phiAsPOI":
-            if 'phiAsPOI' in po: 
-                print "Will consider the phase as a parameter of interest"
-                self.phiPOI = True
-                self.phiFloating = True
-            if 'phiFloating' in po: 
-                print "Will consider the phase as a floating parameter"
-                self.phiFloating = True
+                self.muAsPOI = False
+                self.fai1Floating = True
+                self.fai1POI = True
+                self.phiai1Floating = False
+                self.phiai1POI = False
+                self.fai2Floating = False
+                self.fai2POI = False
+                self.phiai2Floating = False
+                self.phiai2POI = False
             
     def doParametersOfInterest(self):
         """Create POI and other parameters, and define the POI set."""
-        if self.fg4fixed:
-            self.modelBuilder.doVar("CMS_zz4l_fg4[0]")
-            self.modelBuilder.doVar("r[1,0,4]")
-            print "Fixing CMS_zz4l_fg4"
-            poi = "r"
+        if self.fai1Floating:
+            self.modelBuilder.doVar("CMS_zz4l_fai1[0.,0,1]")
+            print "Floating CMS_zz4l_fai1"
+            if self.fai1POI:
+                poi = "CMS_zz4l_fai1"
         else:
-            if self.modelBuilder.out.var("CMS_zz4l_fg4"):
-                print "have fg4 inside"
-            else:
-                self.modelBuilder.doVar("CMS_zz4l_fg4[0.,0,1]")
-            poi = "CMS_zz4l_fg4"
+            self.modelBuilder.doVar("CMS_zz4l_fai1[0]")
+            print "Fixing CMS_zz4l_fai1"
+                
+        if self.fai2Floating:
+            self.modelBuilder.doVar("CMS_zz4l_fai2[0.,0,1]")
+            print "Floating CMS_zz4l_fai2"
+            if self.fai2POI:
+                if self.fai1POI:
+                    poi += ",CMS_zz4l_fai2"
+                else:
+                    poi = "CMS_zz4l_fai2"
+        else:
+            self.modelBuilder.doVar("CMS_zz4l_fai2[0]")
+            print "Fixing CMS_zz4l_fai2"
 
-            if self.muFloating:
-                self.modelBuilder.doVar("r[1,0,4]")
-                if self.muAsPOI:
-                    print "Treating r as a POI"
+        if self.muFloating:
+            self.modelBuilder.doVar("r[1,0,5]")
+            if self.muAsPOI:
+                print "Treating r as a POI"
+                if self.fai1POI or self.fai2POI:
                     poi += ",r"
                 else:
-                    self.modelBuilder.out.var("r").setAttribute("flatParam")
-            if self.phiFloating:
-                #self.modelBuilder.doVar("CMS_zz4l_fg4phi[0.,-math.pi,math.pi]")
-                if self.modelBuilder.out.var("CMS_zz4l_fg4phi"):
-                    print "have fg4phi inside"
-                else: 
-                    self.modelBuilder.doVar("CMS_zz4l_fg4phi[0.,-3.1415926,3.1415926]")
-                if self.phiPOI:
-                    poi += ",CMS_zz4l_fg4phi"
+                    poi = "r"
+            else:
+                self.modelBuilder.out.var("r").setAttribute("flatParam")
+
+        if self.phiai1Floating:
+            self.modelBuilder.doVar("CMS_zz4l_phiai1[0.,0,1]")
+            print "Floating CMS_zz4l_phiai1"
+            if self.phiai1POI:
+                print "Treating phiai1 as a POI"
+                if self.fai1POI or self.fai2POI or self.muAsPOI:
+                    poi += ",CMS_zz4l_phiai1"
                 else:
-                    self.modelBuilder.out.var("CMS_zz4l_fg4phi").setAttribute("flatParam")
-        
+                    poi = "CMS_zz4l_phiai1"
+        else:
+            self.modelBuilder.doVar("CMS_zz4l_phiai1[0]")
+            print "Fixing CMS_zz4l_phiai1"
+
+        if self.phiai2Floating:
+            self.modelBuilder.doVar("CMS_zz4l_phiai2[0.,0,1]")
+            print "Floating CMS_zz4l_phiai2"
+            if self.phiai2POI:
+                print "Treating phiai2 as a POI"
+                if self.fai1POI or self.fai2POI or self.muAsPOI or self.phiai1POI:
+                    poi += ",CMS_zz4l_phiai2"
+                else:
+                    poi = "CMS_zz4l_phiai2"
+        else:
+            self.modelBuilder.doVar("CMS_zz4l_phiai2[0]")
+            print "Fixing CMS_zz4l_phiai2"
+            
         self.modelBuilder.doSet("POI",poi)
         
 spinZeroHiggs = SpinZeroHiggs()
