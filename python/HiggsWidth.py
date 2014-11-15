8cf6ce36a1c2ee8a2a51bc7aa36f6e7794870b77
usarica <ulascan90@gmail.com>
2014-11-14 20:23:16 -0500

    Why is the latest model not here already?

--- python/HiggsWidth.py
+++ python/HiggsWidth.py
@@ -8,6 +8,8 @@ class Higgswidth(PhysicsModel):
         self.GGsmfixed = False
         self.RVRFfixed = False
         self.GGsmRVRFfixed = False
+        self.hasACfai1 = False
+        self.forbidPMF = False
         self.is2l2nu = False
         self.poiMap = []
         self.pois = {}
@@ -28,7 +30,6 @@ class Higgswidth(PhysicsModel):
                 return "R"
             else:
                 return "RF"
-                
         elif process in ["qqH","WH","ZH","VH"]:
             if self.RVRFfixed or self.GGsmRVRFfixed:
                 return "R"
@@ -39,39 +40,50 @@ class Higgswidth(PhysicsModel):
             
     def setPhysicsOptions(self,physOptions):
         for po in physOptions:
-            if po == "GGsmfixed":
+            if 'GGsmfixed' in po:
                 print "Will fix CMS_zz4l_GGsm to 1 and float muV, muF"
                 self.GGsmfixed = True
-            if po == "RVRFfixed":
+            if 'RVRFfixed' in po:
                 print "Will fix muV, muF to 1 and float mu"
                 self.RVRFfixed = True
-            if po == "GGsmRVRFfixed":
+            if 'GGsmRVRFfixed' in po:
                 print "Will fix muV, muF, CMS_zz4l_GGsm to 1 and float mu"
                 self.GGsmRVRFfixed = True
-            if po == "is2l2nu":
+            if 'is2l2nu' in po:
                 print "Will consider cards in 2l2nu style (separated S, B, S+B+I)"
                 self.is2l2nu = True
+            if 'ACfai1' in po:
+                print "Model will consider fai1 for anomalous couplings onshell and offshell. Notice that it is not going to be the single POI"
+                self.hasACfai1 = True
+                if 'forbidPMF' in po:
+                    print "Negative real phase will be forbidden for anomalous couplings"
+                    self.forbidPMF = True
             
     def doParametersOfInterest(self):
         """Create POI and other parameters, and define the POI set."""
-        self.modelBuilder.doVar("R[1.,0.,4]")
-        self.modelBuilder.doVar("RV[1.,0.,4]")
-        self.modelBuilder.doVar("RF[1.,0.,4]")
-        self.modelBuilder.doVar("CMS_zz4l_GGsm[1.,0.,50.]")
-        self.modelBuilder.doVar("CMS_widthH_kbkg[1.,0.,2.]")
-	self.modelBuilder.out.var("R").setVal(1)
-	self.modelBuilder.out.var("RV").setVal(1)
-	self.modelBuilder.out.var("RF").setVal(1)
-	self.modelBuilder.out.var("CMS_zz4l_GGsm").setVal(1)
-	self.modelBuilder.out.var("CMS_widthH_kbkg").setVal(1)
+        if not self.modelBuilder.out.var("R"):
+            self.modelBuilder.doVar("R[1.,0.,100.]")
+        if not self.modelBuilder.out.var("RV"):
+            self.modelBuilder.doVar("RV[1.,0.,100.]")
+        if not self.modelBuilder.out.var("RF"):
+            self.modelBuilder.doVar("RF[1.,0.,100.]")
+        if not self.modelBuilder.out.var("CMS_zz4l_GGsm"):
+            self.modelBuilder.doVar("CMS_zz4l_GGsm[1.,0.,50.]")
+        if not self.modelBuilder.out.var("CMS_widthH_kbkg"):
+            self.modelBuilder.doVar("CMS_widthH_kbkg[1.,0.,2.]")
+        self.modelBuilder.out.var("R").setVal(1)
+        self.modelBuilder.out.var("RV").setVal(1)
+        self.modelBuilder.out.var("RF").setVal(1)
+        self.modelBuilder.out.var("CMS_zz4l_GGsm").setVal(1)
+        self.modelBuilder.out.var("CMS_widthH_kbkg").setVal(1)
 
-	if self.GGsmfixed:
-	    self.modelBuilder.out.var("CMS_zz4l_GGsm").setConstant(True)
+        if self.GGsmfixed:
+            self.modelBuilder.out.var("CMS_zz4l_GGsm").setConstant(True)
             print "Fixing CMS_zz4l_GGsm and R"
             self.modelBuilder.out.var("R").setConstant(True)
             poi = "RV,RF"
-	elif self.GGsmRVRFfixed:
-	    self.modelBuilder.out.var("CMS_zz4l_GGsm").setConstant(True)
+        elif self.GGsmRVRFfixed:
+            self.modelBuilder.out.var("CMS_zz4l_GGsm").setConstant(True)
             print "Fixing CMS_zz4l_GGsm and RV, RF"
             self.modelBuilder.out.var("RV").setConstant(True)
             self.modelBuilder.out.var("RF").setConstant(True)
@@ -84,7 +96,22 @@ class Higgswidth(PhysicsModel):
                 self.modelBuilder.out.var("R").setConstant(True)
             poi = "CMS_zz4l_GGsm"
 
-	self.modelBuilder.factory_("expr::ggH_s_func(\"@0*@1*@3-sqrt(@0*@1*@2*@3)\",R,CMS_zz4l_GGsm,CMS_widthH_kbkg,RF)")
+        if self.hasACfai1:
+            if not self.modelBuilder.out.var("CMS_zz4l_fai1"):
+                print "Could not detect fai1, building a new one"
+                self.modelBuilder.doVar("CMS_zz4l_fai1[0,-1.,1.]")
+            self.modelBuilder.out.var("CMS_zz4l_fai1").setVal(0)
+            if self.forbidPMF:
+                print "fai1 cannot fall below 0"
+                self.modelBuilder.out.var("CMS_zz4l_fai1").setRange(0,1)
+            poi += ",CMS_zz4l_fai1"
+        else:
+            if self.modelBuilder.out.var("CMS_zz4l_fai1"):
+                print "Found fai1 but will fix it to 0"
+                self.modelBuilder.out.var("CMS_zz4l_fai1").setVal(0)
+                self.modelBuilder.out.var("CMS_zz4l_fai1").setConstant()
+
+        self.modelBuilder.factory_("expr::ggH_s_func(\"@0*@1*@3-sqrt(@0*@1*@2*@3)\",R,CMS_zz4l_GGsm,CMS_widthH_kbkg,RF)")
         self.modelBuilder.factory_("expr::ggH_b_func(\"@2-sqrt(@0*@1*@2*@3)\",R,CMS_zz4l_GGsm,CMS_widthH_kbkg,RF)")
         self.modelBuilder.factory_("expr::ggH_sbi_func(\"sqrt(@0*@1*@2*@3)\",R,CMS_zz4l_GGsm,CMS_widthH_kbkg,RF)")
 
