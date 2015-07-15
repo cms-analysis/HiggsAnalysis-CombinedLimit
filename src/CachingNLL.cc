123d697ec52382f08c6e011121e5ead3f4ae72b4
Andrea Marini <andrea.marini@cern.ch>
2015-07-15 14:31:38 +0200

    add a protection for the 0 dataset that factorized pdf exists

--- src/CachingNLL.cc
+++ src/CachingNLL.cc
@@ -814,6 +814,9 @@ cacheutils::CachingSimNLL::setup_()
 
     RooArgList constraints;
     factorizedPdf_.reset(dynamic_cast<RooSimultaneous *>(utils::factorizePdf(*dataOriginal_->get(), *pdfclone, constraints)));
+
+    std::cout<<"[cacheutils]::[CachingSimNLL]::[setup_]::[DEBUG] factorizedPDF"<<endl;
+    factorizedPdf_->Print("V");
     
     RooSimultaneous *simpdf = factorizedPdf_.get();
     constrainPdfs_.clear(); 
@@ -844,6 +847,7 @@ cacheutils::CachingSimNLL::setup_()
         params->Print("V");
         std::cout << "Obs: " << std::endl;
         dataOriginal_->get()->Print("V");
+	std::cout<<" WARNING RELEASE !!!"<<std::endl;
         factorizedPdf_.release();
         simpdf = dynamic_cast<RooSimultaneous *>(pdfclone);
     }
@@ -853,6 +857,7 @@ cacheutils::CachingSimNLL::setup_()
     pdfs_.resize(catClone->numBins(NULL), 0);
     //dataSets_.reset(dataOriginal_->split(pdfOriginal_->indexCat(), true));
     datasets_.resize(pdfs_.size(), 0);
+    std::cout<<" AGAIN BEFORE SPLIT"<<endl;
     splitWithWeights(*dataOriginal_, simpdf->indexCat(), true);
     //std::cout << "Pdf " << simpdf->GetName() <<" is a SimPdf over category " << catClone->GetName() << ", with " << pdfs_.size() << " bins" << std::endl;
     for (int ib = 0, nb = pdfs_.size(); ib < nb; ++ib) {
@@ -951,7 +956,7 @@ void cacheutils::CachingSimNLL::splitWithWeights(const RooAbsData &data, const R
     RooArgSet obsplus(obs); obsplus.add(weight);
     if (nb != int(datasets_.size())) throw std::logic_error("Number of categories changed"); // this can happen due to bugs in RooDataSet
     std::vector<int> includeZeroWeights(nb,0); bool includeZeroWeightsAny = false;
-    if (runtimedef::get("ADDNLL_ROOREALSUM_BASICINT") && runtimedef::get("ADDNLL_ROOREALSUM_KEEPZEROS")) {
+    if (runtimedef::get("ADDNLL_ROOREALSUM_BASICINT") && runtimedef::get("ADDNLL_ROOREALSUM_KEEPZEROS") and factorizedPdf_.get() != NULL ) {
         for (int ib = 0; ib < nb; ++ib) {
             catClone->setBin(ib);
             RooAbsPdf *pdf = factorizedPdf_->getPdf(catClone->getLabel());
