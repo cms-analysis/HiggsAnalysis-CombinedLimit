bfdae199cc9ac05ca6f4e588de6033e5dd6f4986
cecilecaillol <cecilecaillol@gmail.com>
2015-09-29 17:56:01 +0200

    add TnP physics model

---
+++ python/TagAndProbeModel.py
@@ -0,0 +1,43 @@
+from HiggsAnalysis.CombinedLimit.PhysicsModel import *
+import re
+
+class TagAndProbe(PhysicsModel):
+    def setPhysicsOptions(self,physOptions):
+        for po in physOptions:
+            if po.startswith("anticorrelation="):
+                self.anticorrelation = float(po.replace("anticorrelation=",""))
+    def doParametersOfInterest(self):
+        """Create POI and other parameters, and define the POI set."""
+        self.modelBuilder.doVar("SF[1.0,0.0,1.2]");
+        self.modelBuilder.doSet("POI","SF")
+        if self.options.mass != 0:
+            if self.modelBuilder.out.var("MH"):
+              self.modelBuilder.out.var("MH").removeRange()
+              self.modelBuilder.out.var("MH").setVal(self.options.mass)
+            else:
+              self.modelBuilder.doVar("MH[%g]" % self.options.mass);
+        exp_pass=1
+        exp_fail=1
+        for b in self.DC.bins:
+           for p in self.DC.exp[b].keys():
+              if self.DC.isSignal[p]:
+                 if re.search("pass",b):
+                    exp_pass=self.DC.exp[b][p]
+                 if re.search("fail",b):
+                    exp_fail=self.DC.exp[b][p]
+        self.modelBuilder.factory_('expr::fail_scale("(%f+%f-(%f*@0))/%f", SF)'%(exp_pass,exp_fail,exp_pass,exp_fail))
+    def getYieldScale(self,bin,process):
+        "Return the name of a RooAbsReal to scale this yield by or the two special values 1 and 0 (don't scale, and set to zero)"
+        if self.DC.isSignal[process]:
+                if re.search("pass",bin):
+                        return "SF"
+                elif re.search("fail",bin):
+                        return "fail_scale"
+                else:
+                        poi=1
+                        return int(poi)
+        else:
+                poi=1
+                return int(poi)
+
+tagAndProbe = TagAndProbe()
