3f04c9b121d89109f7546bf6c20d8f8860570695
nckw <n.c.k.wardle@googlemail.com>
2015-11-20 10:43:35 +0100

    Fix issue combining multi-bin with single-bin
    When combining multi-bin datacards which use `rateParams`, the bin label was being dropped in the `rateParam` directive. This is fixed now but the old (correct) behaviour is still maintained for single bin cards.

--- scripts/combineCards.py
+++ scripts/combineCards.py
@@ -114,7 +114,8 @@ for ich,fname in enumerate(args):
     # rate params
     for K in DC.rateParams.iterkeys():
 	tbin,tproc = K.split("AND")[0],K.split("AND")[1]
-	tbin = label; nK = tbin+"AND"+tproc
+	tbin = label if singlebin else label+tbin 
+	nK = tbin+"AND"+tproc
 	rateParams[nK] = DC.rateParams[K]
     # discrete nuisance
     for K in DC.discretes: 
