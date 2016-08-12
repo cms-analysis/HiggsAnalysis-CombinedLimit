c1cd871e529b1ea81a94a6d59a910afd4969373f
Ulascan Sarica <ulascan90@gmail.com>
2016-08-12 09:56:09 -0400

    revert some changes

--- src/MultiDimFit.cc
+++ src/MultiDimFit.cc
@@ -563,24 +563,16 @@ void MultiDimFit::doGrid(RooWorkspace *w, RooAbsReal &nll)
         RooAbsReal::setEvalErrorLoggingMode(RooAbsReal::CountErrors);
         CloseCoutSentry sentry(verbose < 2);
         double deltaX =  (pmax[0]-pmin[0])/sqrn, deltaY = (pmax[1]-pmin[1])/sqrn;
-        for (unsigned int i = 0; i <= sqrn; ++i) {
-            for (unsigned int j = 0; j <= sqrn; ++j, ++ipoint) {
+        for (unsigned int i = 0; i < sqrn; ++i) {
+            for (unsigned int j = 0; j < sqrn; ++j, ++ipoint) {
                 if (ipoint < firstPoint_) continue;
                 if (ipoint > lastPoint_)  break;
                 *params = snap; 
-                double x =  pmin[0] + (i)*deltaX; 
-                double y =  pmin[1] + (j)*deltaY; 
+                double x =  pmin[0] + (i+0.5)*deltaX; 
+                double y =  pmin[1] + (j+0.5)*deltaY; 
                 if (verbose && (ipoint % nprint == 0)) {
                          fprintf(sentry.trueStdOut(), "Point %d/%d, (i,j) = (%d,%d), %s = %f, %s = %f\n",
                                         ipoint,sqrn*sqrn, i,j, poiVars_[0]->GetName(), x, poiVars_[1]->GetName(), y);
-                }
-                // shift points at the edge |x|+|y|=1
-                if( fabs(x)+fabs(y)>(1.-deltaX/2.) && fabs(x)+fabs(y)<(1.+deltaX/2.) )// consider that deltaX=deltaY
-                {
-                    if(x>0.) x -= 0.00001;
-                    else if(x<0.) x += 0.00001;
-                    if(y>0.) y -= 0.00001;
-                    else if(y<0.) y += 0.00001;
                 }
                 poiVals_[0] = x;
                 poiVals_[1] = y;
