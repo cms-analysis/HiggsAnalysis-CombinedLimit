a33e31110d93f0302a7899dafbe5d7f141aec710
edjtscott <e.scott15@ic.ac.uk>
2017-09-21 12:55:35 +0200

    add bbH process to SMHiggsBuilder, fix how process,decay are obtained (#410)

--- python/PhysicsModel.py
+++ python/PhysicsModel.py
@@ -131,7 +131,7 @@ def getHiggsProdDecMode(bin,process,options):
     processSource = process
     decaySource   = options.fileName+":"+bin # by default, decay comes from the datacard name or bin label
     if "_" in process: 
-        (processSource, decaySource) = "_".join(process.split("_")[0]),process.split("_")[-1] # ignore anything in the middle for SM-like higgs
+        (processSource, decaySource) = process.split("_")[0],process.split("_")[-1] # ignore anything in the middle for SM-like higgs
         if decaySource not in ALL_HIGGS_DECAYS:
             print "ERROR", "Validation Error: signal process %s has a postfix %s which is not one recognized higgs decay modes (%s)" % (process,decaySource,ALL_HIGGS_DECAYS)
             #raise RuntimeError, "Validation Error: signal process %s has a postfix %s which is not one recognized higgs decay modes (%s)" % (process,decaySource,ALL_HIGGS_DECAYS)
