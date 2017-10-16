d608afb1730d6aca7c7fc3f3e7c7d68655889667
nucleosynthesis <n.c.k.wardle@googlemail.com>
2017-10-16 13:48:01 +0200

    floatNuisances -> floatParameters

--- src/Combine.cc
+++ src/Combine.cc
@@ -134,7 +134,7 @@ Combine::Combine() :
       ("validateModel,V", "Perform some sanity checks on the model and abort if they fail.")
       ("saveToys",   "Save results of toy MC in output file")
       ("floatAllNuisances", po::value<bool>(&floatAllNuisances_)->default_value(false), "Make all nuisance parameters floating")
-      ("floatNuisances", po::value<string>(&floatNuisances_)->default_value(""), "Set to floating these nuisance parameters (note freeze will take priority over float)")
+      ("floatParameters", po::value<string>(&floatNuisances_)->default_value(""), "Set to floating these parameters (note freeze will take priority over float)")
       ("freezeAllGlobalObs", po::value<bool>(&freezeAllGlobalObs_)->default_value(true), "Make all global observables constant")
       ;
     miscOptions_.add_options()
@@ -578,7 +578,7 @@ void Combine::run(TString hlfFile, const std::string &dataset, double &limit, do
 
   if (floatNuisances_ != "") {
       RooArgSet toFloat((floatNuisances_=="all")?*nuisances:(w->argSet(floatNuisances_.c_str())));
-      if (verbose > 0) std::cout << "Set floating the following nuisance parameters: "; toFloat.Print("");
+      if (verbose > 0) std::cout << "Set floating the following parameters: "; toFloat.Print("");
       utils::setAllConstant(toFloat, false);
   }
   
