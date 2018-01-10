672805b9603e19c901b748913dd7daea14687b35
nucleosynthesis <n.c.k.wardle@googlemail.com>
2018-01-10 14:54:08 +0100

    Add pcre library

--- env_standalone.sh
+++ env_standalone.sh
@@ -1,10 +1,11 @@
- . /cvmfs/cms.cern.ch/slc6_amd64_gcc491/external/gcc/4.9.1-cms/etc/profile.d/init.sh 
- cd /cvmfs/cms.cern.ch/slc6_amd64_gcc491/lcg/root/6.02.00-odfocd5
+ . /cvmfs/cms.cern.ch/slc6_amd64_gcc530/external/gcc/5.3.0/etc/profile.d/init.sh 
+ cd /cvmfs/cms.cern.ch/slc6_amd64_gcc530/lcg/root/6.06.00-cms/
  . ./bin/thisroot.sh
  cd -
- . /cvmfs/cms.cern.ch/slc6_amd64_gcc491/cms/vdt/v0.3.2-cms/etc/profile.d/init.sh 
- . /cvmfs/cms.cern.ch/slc6_amd64_gcc491/external/boost/1.51.0-cms/etc/profile.d/init.sh 
- . /cvmfs/cms.cern.ch/slc6_amd64_gcc491/external/xz/5.2.1/etc/profile.d/init.sh 
+ . /cvmfs/cms.cern.ch/slc6_amd64_gcc530/cms/vdt/v0.3.2-giojec/etc/profile.d/init.sh 
+ . /cvmfs/cms.cern.ch/slc6_amd64_gcc530/external/boost/1.57.0-giojec/etc/profile.d/init.sh 
+ . /cvmfs/cms.cern.ch/slc6_amd64_gcc530/external/xz/5.2.1/etc/profile.d/init.sh 
+ export LD_LIBRARY_PATH=/cvmfs/cms.cern.ch/slc6_amd64_gcc530/external/pcre/8.37/lib/:${LD_LIBRARY_PATH}
  export PATH=${PATH}:${PWD}/exe:${PWD}/scripts
  export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${PWD}/lib
  export PYTHONPATH=${PYTHONPATH}:${PWD}/lib/python:${PWD}/lib
