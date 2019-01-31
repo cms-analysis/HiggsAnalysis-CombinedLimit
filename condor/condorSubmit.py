4ef3c327c7d240ebd0761b24a23383ac87eae717
Christopher_Madrid <Chris_Madrid@Baylor.edu>
2019-01-30 20:48:16 -0600

    Added running toy fits: not sure if it will work yet

--- condor/condorSubmit.py
+++ condor/condorSubmit.py
@@ -1,13 +1,12 @@
-#!/cvmfs/cms.cern.ch/slc6_amd64_gcc630/cms/cmssw/CMSSW_9_3_3/external/slc6_amd64_gcc630/bin/python
-####!${SRT_CMSSW_RELEASE_BASE_SCRAMRTDEL}/external/${SCRAM_ARCH}/bin/python
-
 import sys, os
 from os import system, environ
 import optparse 
 import subprocess
+import time
 
 def main():
     repo = "HiggsAnalysis/CombinedLimit"
+    seed = int(time.time())
     
     # Parse command line arguments
     parser = optparse.OptionParser("usage: %prog [options]\n")
@@ -68,38 +67,68 @@ def main():
             if not os.path.isdir("%s/output-files/%s" % (options.outPath, outDir)):
                 os.makedirs("%s/output-files/%s" % (options.outPath, outDir))
     
-            outputFiles = [
-                "higgsCombine%s.AsymptoticLimits.mH%s.MODEL%s.root" % (options.year, mass, st),
-                "higgsCombine%s%s%s.FitDiagnostics.mH%s.MODEL%s.root" % (options.year, st, mass, mass, st),
-                "higgsCombine%s%s%s_SignifExp.Significance.mH%s.MODEL%s.root" % (options.year, st, mass, mass, st),
-                "higgsCombineSCAN_r_wSig.MultiDimFit.mH%s.MODEL%s.root " % (mass, st),
-                "higgsCombine%s.HybridNew.mH%s.MODEL%s.root" % (options.year, mass, st),
-                "MVA_%s_%s_%s_ws.root"   % (options.year, st, mass),
-                "MVA_2016_%s_%s_ws.root" % (st, mass),
-                "MVA_2017_%s_%s_ws.root" % (st, mass),
-                "ws_%s_%s_%s.root"       % (options.year, st, mass),
-                "fitDiagnostics%s%s%s.root" % (options.year, st, mass), 
-                "log_%s%s%s_Asymp.txt"      % (options.year, st, mass),
-                "log_%s%s%s_FitDiag.txt"    % (options.year, st, mass),
-                "log_%s%s%s_Sign_sig.txt"   % (options.year, st, mass),
-                "log_%s%s%s_Sign_noSig.txt" % (options.year, st, mass),
-                "log_%s%s%s_multiDim.txt"   % (options.year, st, mass),
-                "log_%s%s%s_HybridNew.txt"  % (options.year, st, mass),
-                           ]
-    
-            transfer = "transfer_output_remaps = \""
-            for f in outputFiles:
-                transfer += "%s = %s/output-files/%s/%s" % (f, options.outPath, outDir, f)
-                if f != outputFiles[-1]:
-                    transfer += "; "
-            transfer += "\"\n"
-    
-            fileParts.append(transfer)
-            fileParts.append("Arguments = %s %s %s %s %s %s\n" % (options.inputRoot2016, options.inputRoot2017, st, mass, options.year, options.dataType))
-            fileParts.append("Output = %s/log-files/MyFit_%s_%s.stdout\n"%(options.outPath, st, mass))
-            fileParts.append("Error = %s/log-files/MyFit_%s_%s.stderr\n"%(options.outPath, st, mass))
-            fileParts.append("Log = %s/log-files/MyFit_%s_%s.log\n"%(options.outPath, st, mass))
-            fileParts.append("Queue\n\n")
+            if not options.toy:
+                outputFiles = [
+                    "higgsCombine%s.AsymptoticLimits.mH%s.MODEL%s.root" % (options.year, mass, st),
+                    "higgsCombine%s%s%s.FitDiagnostics.mH%s.MODEL%s.root" % (options.year, st, mass, mass, st),
+                    "higgsCombine%s%s%s_SignifExp.Significance.mH%s.MODEL%s.root" % (options.year, st, mass, mass, st),
+                    "higgsCombineSCAN_r_wSig.MultiDimFit.mH%s.MODEL%s.root " % (mass, st),
+                    "higgsCombine%s.HybridNew.mH%s.MODEL%s.root" % (options.year, mass, st),
+                    "MVA_%s_%s_%s_ws.root"   % (options.year, st, mass),
+                    "MVA_2016_%s_%s_ws.root" % (st, mass),
+                    "MVA_2017_%s_%s_ws.root" % (st, mass),
+                    "ws_%s_%s_%s.root"       % (options.year, st, mass),
+                    "fitDiagnostics%s%s%s.root" % (options.year, st, mass), 
+                    "log_%s%s%s_Asymp.txt"      % (options.year, st, mass),
+                    "log_%s%s%s_FitDiag.txt"    % (options.year, st, mass),
+                    "log_%s%s%s_Sign_sig.txt"   % (options.year, st, mass),
+                    "log_%s%s%s_Sign_noSig.txt" % (options.year, st, mass),
+                    "log_%s%s%s_multiDim.txt"   % (options.year, st, mass),
+                    "log_%s%s%s_HybridNew.txt"  % (options.year, st, mass),
+                ]
+            
+                transfer = "transfer_output_remaps = \""
+                for f in outputFiles:
+                    transfer += "%s = %s/output-files/%s/%s" % (f, options.outPath, outDir, f)
+                    if f != outputFiles[-1]:
+                        transfer += "; "
+                transfer += "\"\n"
+                    
+                fileParts.append(transfer)
+                fileParts.append("Arguments = %s %s %s %s %s %s\n" % (options.inputRoot2016, options.inputRoot2017, st, mass, options.year, options.dataType))
+                fileParts.append("Output = %s/log-files/MyFit_%s_%s.stdout\n"%(options.outPath, st, mass))
+                fileParts.append("Error = %s/log-files/MyFit_%s_%s.stderr\n"%(options.outPath, st, mass))
+                fileParts.append("Log = %s/log-files/MyFit_%s_%s.log\n"%(options.outPath, st, mass))
+                fileParts.append("Queue\n\n")
+            else:
+                nSteps = int(round((options.rMax - options.rMin)/options.rStep))
+                for x in range(0, nSteps+1):           
+                    r = options.rMin + float(x)*options.rStep 
+                    print "    r = ", r
+                
+                    for y in range(options.jPerR):                        
+                        print "        seed = ", -1*seed
+
+                        outputFiles = [
+                            "higgsCombine%s.HybridNew.mH%s.MODEL%s.%s.root" % (options.year, mass, st, str(-1*seed)),
+                            "log_%s%s%s_%s_%s_HybridNew.txt" % (options.year, st, mass, str(r), str(-1*seed)),
+                        ]
+
+                        transfer = "transfer_output_remaps = \""
+                        for f in outputFiles:
+                            transfer += "%s = %s/output-files/%s/%s" % (f, options.outPath, outDir, f)
+                            if f != outputFiles[-1]:
+                                transfer += "; "
+                        transfer += "\"\n"
+
+                        fileParts.append(transfer)
+                        fileParts.append("Arguments = %s %s %s %s %s %s %s %s %s\n" % (options.inputRoot2016, options.inputRoot2017, st, mass, options.year, 
+                                                                                       options.dataType, str(r), str(-1*seed), str(options.numToys)))
+                        fileParts.append("Output = %s/log-files/MyFit_%s_%s_%s_%s.stdout\n"%(options.outPath, st, mass, str(r), str(-1*seed)))
+                        fileParts.append("Error = %s/log-files/MyFit_%s_%s_%s_%s.stderr\n"%(options.outPath, st, mass, str(r), str(-1*seed)))
+                        fileParts.append("Log = %s/log-files/MyFit_%s_%s_%s_%s.log\n"%(options.outPath, st, mass, str(r), str(-1*seed)))
+                        fileParts.append("Queue\n\n")
+                        seed+=1
     
     fout = open("condor_submit.txt", "w")
     fout.write(''.join(fileParts))
