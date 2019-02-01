0bcd1e80a8f337f5866f60769253e28941a79d83
Christopher_Madrid <Chris_Madrid@Baylor.edu>
2019-02-01 11:51:45 -0600

    hadder script works well now

--- condor/hadder.py
+++ condor/hadder.py
@@ -22,33 +22,28 @@ def main():
     parser.add_option ('-t', dest='dataType',       type='string', default = 'data',            help="Specify if running over data or sudo data")
     parser.add_option ('-m', dest='masssets',       type='string', default = '',                help="List of mass models, comma separated")
     parser.add_option ('-y', dest='year',           type='string', default = '2016',            help="year")
-    parser.add_option ('-p', dest='inPath',         type='string', default='output-files',      help="Can pass in the input directory name")
+    parser.add_option ('-p', dest='inPath',         type='string', default ='??',               help="Can pass in the input directory name")
     parser.add_option ('-o', action='store_true',                                               help="Overwrite output directory")
 
     options, args = parser.parse_args()    
     signalType = getOptionList(options.signalType, "No dataset specified")
     masssets = getOptionList(options.masssets, "No mass model specified")
-    
-    # Get input directory path
+    overwrite = '-f' if options.o else ''
+
     inPath = options.inPath
     for st in signalType:
         st = st.strip()
         for mass in masssets:
             mass = mass.strip()
+            print "-----------------------------------------------------------"
             print st, mass, options.year
-    
-            # create the directory
-            outDir = st+"_"+mass+"_"+options.year
-            if not os.path.isdir("%s/output-files/%s" % (options.outPath, outDir)):
-                os.makedirs("%s/output-files/%s" % (options.outPath, outDir))
-    
-    
-    # Loop over all sample options to find files to hadd
-    for ds in datasets:
-        #higgsCombine%s.HybridNew.mH%s.MODEL%s.%s.root" % (options.year, mass, st, str(seed))
-        command = "hadd %s/%s.root %s/%s/*" % (outDir, sampleCollection, inPath, sampleCollection)
-        print command
-        #system(command)
+            print "-----------------------------------------------------------"
 
+            path = options.inPath+"/output-files/"+st+"_"+mass+"_"+options.year
+            infiles = "higgsCombine%s.HybridNew.mH%s.MODEL%s.*.root" % (options.year, mass, st)
+            outfile = "toys_%s_%s_%s_data.root" % (st, mass, options.year)
+            command = "hadd %s %s/%s %s/%s" % (overwrite, path, outfile, path, infiles)
+            system(command)
+            
 if __name__ == "__main__":
     main()
