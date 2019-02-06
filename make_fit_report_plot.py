ab52a22ababc884ec037d009ff5b1c44ac66f444
Christopher_Madrid <Chris_Madrid@Baylor.edu>
2019-02-06 17:20:18 -0600

    Added path option

--- make_fit_report_plot.py
+++ make_fit_report_plot.py
@@ -4,12 +4,12 @@ masses = ["350","450","550","650","750","850"]
 signalTypes = ["RPV","SYY","SHH"]
 years = ["2016","2017"]
 dataTypes = ["Data","pseudoData"]
-#dataTypes = ["pseudoData"]
+path = "DoubleTTStuff"
 
 for mass in masses:
     for st in signalTypes:
         for year in years:
             for dt in dataTypes:
-                command = "root -l -q -b 'fit_report_ESM.C(\"condor/Fit_{3}_{0}/output-files/{2}_{1}_{0}/fitDiagnostics{0}{2}{1}.root\",\"condor/Fit_{3}_{0}/output-files/{2}_{1}_{0}/\")'".format(year,mass,st,dt)
+                command = "root -l -q -b 'fit_report_ESM.C(\"condor/{4}/Fit_{3}_{0}/output-files/{2}_{1}_{0}/fitDiagnostics{0}{2}{1}.root\",\"condor/{4}/Fit_{3}_{0}/output-files/{2}_{1}_{0}/\")'".format(year,mass,st,dt,path)
                 print command
                 os.system(command)
