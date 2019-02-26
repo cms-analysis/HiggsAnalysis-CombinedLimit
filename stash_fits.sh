3308a3b0c7575d1233a5b791d5e98b322f1a3f49
Nadja <nadja.strobbe@cern.ch>
2019-02-26 13:55:00 -0600

    add extra cards for ttbar shape systematics, and a small script to stash results

---
+++ stash_fits.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+DIR=$1
+mkdir $DIR
+
+mv  ws_2016_RPV_550.root $DIR
+mv  D1_CMS_th1x_prefit.png $DIR
+mv  D2_CMS_th1x_prefit.png $DIR
+mv  D3_CMS_th1x_prefit.png $DIR
+mv  D4_CMS_th1x_prefit.png $DIR
+mv  D1_CMS_th1x_fit_b.png $DIR
+mv  D2_CMS_th1x_fit_b.png $DIR
+mv  D3_CMS_th1x_fit_b.png $DIR
+mv  D4_CMS_th1x_fit_b.png $DIR
+mv  covariance_fit_b.png $DIR
+mv  D1_CMS_th1x_fit_s.png $DIR
+mv  D2_CMS_th1x_fit_s.png $DIR
+mv  D3_CMS_th1x_fit_s.png $DIR
+mv  D4_CMS_th1x_fit_s.png $DIR
+mv  covariance_fit_s.png $DIR
+mv  combine_logger.out $DIR
+mv  fitDiagnostics2016RPV550.root $DIR
+mv  higgsCombine2016RPV550.FitDiagnostics.mH550.MODELRPV.root $DIR
+mv  log_2016RPV550_FitDiag.txt $DIR
