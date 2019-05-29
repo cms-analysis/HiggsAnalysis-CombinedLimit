04eb14d9d2065693d56bde2d6049b0953e90be3a
Christopher_Madrid <Chris_Madrid@Baylor.edu>
2019-05-29 08:28:28 -0500

    Fixed small error: forgot to multiply NPs for rPrime

--- make_MVA_8bin_ws.C
+++ make_MVA_8bin_ws.C
@@ -25,7 +25,7 @@ void addNPs(stringstream& f, RooArgList& list, const double r, const RooAbsArg& 
 {
     std::string paramNum  = std::to_string(list.getSize());
     std::string paramNumG = std::to_string(list.getSize()+1);
-    f << "*TMath::Power(" << r << ",@"+paramNum+")*TMath::Power(" << rprime << ",@"+paramNumG+")";
+    f << "*TMath::Power(" << r << ",@"+paramNum+")*TMath::Power(" << rprime << ",@"+paramNumG+"*@"+paramNum+")";
     list.add(NP);
     list.add(NPg);
 }
