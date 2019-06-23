0e45f2de5cf6dc33db42364c79ed05e3d479d234
Christopher_Madrid <Chris_Madrid@Baylor.edu>
2019-06-23 08:29:14 -0500

    added a ratio plot and cleaned up

--- condor/table_signal_strength.py
+++ condor/table_signal_strength.py
@@ -7,54 +7,6 @@ import ROOT
 from array import array
 
 def makePValuePlot(dataSet):
-    sohaStyle= ROOT.TStyle("SOHA","soha plot style")
-    
-    # use plain black on white colors
-    sohaStyle.SetFrameBorderMode(0)
-    sohaStyle.SetCanvasBorderMode(0)
-    sohaStyle.SetPadBorderMode(0)
-    sohaStyle.SetPadColor(0)
-    sohaStyle.SetCanvasColor(0)
-    sohaStyle.SetTitleColor(1)
-    sohaStyle.SetStatColor(0)
-
-    # set the paper & margin sizes
-    sohaStyle.SetPaperSize(20,26)
-    sohaStyle.SetPadTopMargin(0.05)
-    sohaStyle.SetPadRightMargin(0.05)
-    sohaStyle.SetPadBottomMargin(0.16)
-    sohaStyle.SetPadLeftMargin(0.12)
-
-    # use large Times-Roman fonts
-    sohaStyle.SetTextFont(132)
-    sohaStyle.SetTextSize(0.08)
-    sohaStyle.SetLabelFont(132,"x")
-    sohaStyle.SetLabelFont(132,"y")
-    sohaStyle.SetLabelFont(132,"z")
-    sohaStyle.SetLabelSize(0.05,"x")
-    sohaStyle.SetTitleSize(0.06,"x")
-    sohaStyle.SetLabelSize(0.05,"y")
-    sohaStyle.SetTitleSize(0.06,"y")
-    sohaStyle.SetLabelSize(0.05,"z")
-    sohaStyle.SetTitleSize(0.06,"z")
-
-    # use bold lines and markers
-    sohaStyle.SetMarkerStyle(8)
-    #sohaStyle.SetHistLineWidth(1.85)
-    sohaStyle.SetLineStyleString(2,"[12 12]") # postscript dashes
-
-    # do not display any of the standard histogram decorations
-    sohaStyle.SetOptTitle(0)
-    sohaStyle.SetOptStat(0)
-    sohaStyle.SetOptFit(0)
-
-    # put tick marks on top and RHS of plots
-    sohaStyle.SetPadTickX(1)
-    sohaStyle.SetPadTickY(1)
-    sohaStyle.SetPadTopMargin(0.10)
-    sohaStyle.SetTitleOffset(-0.1,"x")
-    ROOT.gROOT.SetStyle("SOHA")
-
     # CL observed pvalues
     pvalue_2016 = dataSet["data"]["2016"]["pList"]
     pvalue_2017 = dataSet["data"]["2017"]["pList"]
@@ -63,25 +15,42 @@ def makePValuePlot(dataSet):
     npoints = len(pvalue_2016)
     Xmin = 300
     Xmax = 900
-    Ymin = 0.000000001
+    Ymin = 0.0000005
     Ymax = 1
 
-    c1 = ROOT.TCanvas("c1","Color Octet PValues 8 TeV",800,600)
+    c1 = ROOT.TCanvas("c1","PValues",1000,1000)
+    c1.Divide(1, 2)    
     c1.SetFillColor(0)
-    c1.cd()
+    c1.cd(1)
+    ROOT.gPad.SetPad("p1", "p1", 0, 2.5 / 9.0, 1, 1, ROOT.kWhite, 0, 0)
+    ROOT.gPad.SetBottomMargin(0.01)
+    ROOT.gPad.SetLeftMargin(0.11)
+    ROOT.gPad.SetRightMargin(0.04)
+    ROOT.gPad.SetTopMargin(0.06 * (8.0 / 6.5))
     ROOT.gPad.SetLogy()
+    ROOT.gPad.SetTicks(1,1)
+
+    h = ROOT.TH1F("dummy","dummy",1, 300, 900)
+    h.SetMaximum(Ymax)
+    h.SetMinimum(Ymin)
+    h.SetTitle("")
+    h.SetStats(0)
+    h.GetXaxis().SetLimits(Xmin,Xmax)
+    h.GetXaxis().SetLabelSize(0.05)
+    h.GetXaxis().SetTitleSize(0.05)
+    h.GetYaxis().SetLabelSize(0.05)
+    h.GetYaxis().SetTitleSize(0.05)
+    h.GetYaxis().SetTitleOffset(1.0)
+    #h.GetYaxis().SetRangeUser(0, 2.2)
+    #h.GetYaxis().SetNdivisions(4, 2, 0)
+    h.GetXaxis().SetTitle("m_{#tilde{t}} [GeV]")
+    h.GetYaxis().SetTitle("Local p-value")
+    h.Draw()
 
     gr_Combo = ROOT.TGraph(npoints, array('d', xpoints), array('d', pvalue_Combo))
-    gr_Combo.SetLineColor(1)
+    gr_Combo.SetLineColor(ROOT.kBlack)
     gr_Combo.SetLineWidth(3)
-    gr_Combo.SetMaximum(Ymax)
-    gr_Combo.SetMinimum(Ymin)
-    gr_Combo.GetXaxis().SetLimits(Xmin,Xmax)
-    gr_Combo.GetXaxis().SetTitle("m_{#Theta^{0}} [GeV]")
-    #gr_Combo.GetXaxis().SetTitleSize(0.06)
-    gr_Combo.GetXaxis().SetTitleOffset(1.0)
-    gr_Combo.GetYaxis().SetTitle("local p-value")
-    gr_Combo.Draw("AL")
+    gr_Combo.Draw("same")
 
     gr_2016 = ROOT.TGraph(npoints, array('d', xpoints), array('d', pvalue_2016))
     gr_2016.SetLineColor(ROOT.kRed+1)
@@ -97,7 +66,7 @@ def makePValuePlot(dataSet):
     # For 1 sigma: s = 0.68
     #   1 - (0.5 + s/2) = 0.5 - s/2
     entries = []
-    numSigma = 5
+    numSigma = 4
     for s in range(1, numSigma+1):
         sigma = 0.5 - ROOT.TMath.Erf(s/ROOT.TMath.Sqrt(2))/2
         L = ROOT.TLine(Xmin, sigma, Xmax, sigma)
@@ -105,7 +74,7 @@ def makePValuePlot(dataSet):
         L.SetLineWidth(2)
         L.Draw("same")
 
-        S = ROOT.TPaveText(920,sigma-0.25*sigma,955,sigma+0.5*sigma,"")
+        S = ROOT.TPaveText(900,sigma-0.25*sigma,920,sigma+0.5*sigma,"")
         S.SetBorderSize(0)
         S.SetFillStyle(0)
         S.SetTextColor(2)
@@ -117,24 +86,76 @@ def makePValuePlot(dataSet):
     gr_2017.Draw("L,same")
     gr_Combo.Draw("L,same")
 
-    legend1 = ROOT.TLegend(0.54397,0.183566,0.997487,0.363636,"")
-    legend1.SetHeader("pp #rightarrow #Theta^{0}#Theta^{0} #rightarrow Zgbb (m_{G'} = 2.3 m_{#Theta^{0}})")
-    legend1.AddEntry(gr_Combo, "Combined Observed", "l")
-    legend1.AddEntry(gr_2016, "2016 Observed", "l")
-    legend1.AddEntry(gr_2017, "2017 Observed", "l")
+    legend1 = ROOT.TLegend(0.44397,0.1,0.897487,0.25,"")
+    if dataSet["model"]=="RPV":
+        legend1.SetHeader("pp #rightarrow #tilde{t}#bar{#tilde{t}}, #tilde{t} #rightarrow t #tilde{#chi}^{0}_{1},  #tilde{#chi}^{0}_{1} #rightarrow jjj");
+    elif dataSet["model"]=="SYY":
+        legend1.SetHeader("pp #rightarrow #tilde{t}#bar{#tilde{t}}, SYY coupling");
+    elif dataSet["model"]=="SHH":
+        legend1.SetHeader("pp #rightarrow #tilde{t}#bar{#tilde{t}}, SHH coupling");
+    legend1.AddEntry(gr_Combo, "Combined Observed, L_{Int}=77.4 fb^{-1}", "l")
+    legend1.AddEntry(gr_2016, "2016 Observed, L_{Int}=35.9 fb^{-1}", "l")
+    legend1.AddEntry(gr_2017, "2017 Observed, L_{Int}=41.5 fb^{-1}", "l")
     legend1.SetBorderSize(0)
     legend1.SetFillStyle(0)
     legend1.Draw("same")
 
-    cmstext = ROOT.TPaveText(0.233688, 0.910839, 0.895729, 0.963287, "ndc")
+    cmstext = ROOT.TPaveText(0.325, 0.92, 0.905, 0.98, "ndc")
     cmstext.SetBorderSize(0)
     cmstext.SetFillStyle(0)
     cmstext.SetTextAlign(12)
-    cmstext.AddText("CMS Preliminary, #sqrt{s}=8 TeV, L_{Int}=19.7 fb^{-1}")
+    cmstext.AddText("CMS Preliminary, #sqrt{s}=13 TeV")
     cmstext.Draw("same")
 
     c1.Update()
-    c1.Print("test.pdf")
+    c1.cd(2)
+    ROOT.gPad.SetPad("p2", "p2", 0, 0, 1, 2.5 / 9.0, ROOT.kWhite, 0, 0)
+    ROOT.gPad.SetLeftMargin(0.11)
+    ROOT.gPad.SetRightMargin(0.04)
+    ROOT.gPad.SetTopMargin(0.01)
+    ROOT.gPad.SetBottomMargin(0.37)
+    ROOT.gPad.SetTicks(1,1)
+
+    # Make ratio of Data over total stack
+    hr = ROOT.TH1F("dummyr","dummyr",1, 300, 900)
+    hr.SetStats(0)
+    hr.SetTitle("")
+    hr.GetXaxis().SetTitle("m_{#tilde{t}} [GeV]")
+    hr.GetYaxis().SetTitle("#sigma_{meas.}/#sigma_{pred.}")
+    hr.GetXaxis().SetLimits(Xmin,Xmax)
+    hr.GetXaxis().SetLabelSize(0.14)
+    hr.GetXaxis().SetTitleSize(0.15)
+    hr.GetYaxis().SetLabelSize(0.13)
+    hr.GetYaxis().SetTitleSize(0.15)
+    hr.GetYaxis().SetTitleOffset(0.3)
+    hr.GetYaxis().SetRangeUser(-0.2, 1.2)
+    hr.GetYaxis().SetNdivisions(4, 2, 0)
+    hr.Draw()
+
+    rvalue_Combo = array('d', dataSet["data"]["Combo"]["rList"])
+    rpvalue_Combo = array('d',dataSet["data"]["Combo"]["rpList"])
+    rmvalue_Combo = array('d', dataSet["data"]["Combo"]["rmList"])
+    zero = array('d', dataSet["data"]["2016"]["zero"])
+    rband = ROOT.TGraphAsymmErrors(npoints, array('d', xpoints), rvalue_Combo, zero, zero, rmvalue_Combo, rpvalue_Combo)
+    rband.SetFillColor(ROOT.kGreen+1)
+    rband.Draw("3 same")
+    r = ROOT.TGraph(npoints, array('d', xpoints), rvalue_Combo)
+    r.SetLineColor(ROOT.kBlack)
+    r.SetLineStyle(ROOT.kDashed)
+    r.SetLineWidth(3)
+    r.Draw("PL same")
+    c1.Update()
+
+    line = ROOT.TF1("line", "1", 300, 900)
+    line.SetLineColor(ROOT.kRed)
+    line.Draw("same")
+
+    line2 = ROOT.TF1("line", "1", 300, 900)
+    line2.SetLineColor(ROOT.kBlack)
+    line2.Draw("same")
+
+    c1.Print(dataSet["runtype"]+"_"+dataSet["model"]+".pdf")
+    del c1
 
 def makeSigTex(name, l):    
     f = open(name, 'w')
@@ -162,7 +183,9 @@ def main():
     parser = optparse.OptionParser("usage: %prog [options]\n")
     parser.add_option ('--basedir', dest='basedir',  type='string', default = '.', help="Path to output files")
     options, args = parser.parse_args()
-        
+
+    ROOT.gROOT.SetBatch(True)
+
     pre_tabular = """\\begin{tabular}{l l l l}
     Mass & Best fit signal strength & Observed Significance & p-value\\\\ \hline
     """    
@@ -182,7 +205,7 @@ def main():
             file_table.write(pre_tabular)
             dataSet={"runtype":runtype,"model":model,"data":{}}
             for year in years:
-                data={"mList":[],"rList":[],"rmList":[],"rpList":[],"sList":[],"pList":[]}
+                data={"mList":[],"rList":[],"rmList":[],"rpList":[],"sList":[],"pList":[],"zero":[]}
                 file_table.write("\\multicolumn{4}{c}{%s} \\\\ \\hline \n"%year)
                 for mass in masses:
                     print "Year %s, Model %s, Mass %s"%(year, model, mass)
@@ -225,12 +248,13 @@ def main():
                                 line_pvalue = line_pvalue.replace(")","").strip()
     
                     # Fill lists of data
-                    data["mList"].append(float(mass))
-                    data["rList"].append(float(info_r[0]))
-                    data["rmList"].append(float(info_r[1]))
-                    data["rpList"].append(float(info_r[2]))
-                    data["sList"].append(float(line_sig))
-                    data["pList"].append(float(line_pvalue))
+                    data["mList"].append(abs(float(mass)))
+                    data["rList"].append(abs(float(info_r[0])))
+                    data["rmList"].append(abs(float(info_r[1])))
+                    data["rpList"].append(abs(float(info_r[2])))
+                    data["sList"].append(abs(float(line_sig)))
+                    data["pList"].append(abs(float(line_pvalue)))
+                    data["zero"].append(0.0)
     
                     # Write out r, sigma, and p-value to file
                     if len(info_r) < 3:
@@ -252,12 +276,10 @@ def main():
     makeSigTex("table_signal_strength.tex", l)
     
     for dataSet in d:        
-        print "------------------------------------------"
-        print dataSet["runtype"], dataSet["model"]        
-        for year in dataSet["data"]:
-            print year, dataSet["data"][year]["pList"]
-
-    makePValuePlot(d[0])
+        if dataSet["runtype"] == "Data":
+            print "------------------------------------------"
+            print dataSet["runtype"], dataSet["model"]        
+            makePValuePlot(dataSet)
 
 if __name__ == '__main__':
     main()
