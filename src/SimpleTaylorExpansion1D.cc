ff3b36209c4a29b8a2306f9ca44b5dc3a1380fb6
Giovanni <gpetruc@gmail.com>
2019-08-20 22:28:30 +0200

    text2workspace option to optimize MH-dependent expressions as constants or taylor series

--- src/SimpleTaylorExpansion1D.cc
+++ src/SimpleTaylorExpansion1D.cc
@@ -12,8 +12,8 @@ SimpleTaylorExpansion1D::SimpleTaylorExpansion1D(const char *name, const char *t
 { 
     std::fill_n(ci_, MaxOrder+1, 0.0);
     ci_[0] = func.getVal();
-    if (order > 2) {
-        std::cerr << "Only order 0, 1, 2 are implemented for the moment, out of lazyness" << std::endl;
+    if (order > 4) {
+        std::cerr << "Only order 0..4 are implemented for the moment, out of lazyness" << std::endl;
         assert(false);
     }
     if (order > 0) {
@@ -21,13 +21,29 @@ SimpleTaylorExpansion1D::SimpleTaylorExpansion1D(const char *name, const char *t
         double ym = func.getVal();
         xvar.setVal(x0_ + dx);
         double yp = func.getVal();
-        xvar.setVal(x0_);
 
-        ci_[1] = 0.5*(yp - ym)/dx;
-
-        if (order >= 2) {
-            ci_[2] = ((yp - ci_[0]) + (ym - ci_[0]))/(dx*dx);
+        if (order <= 2) {
+            ci_[1] = 0.5*(yp - ym)/dx;
+            if (order == 2) {
+                ci_[2] = 0.5*((yp - ci_[0]) + (ym - ci_[0]))/(dx*dx);
+            }
+        } else {
+            xvar.setVal(x0_ - 2*dx);
+            double y2m = func.getVal();
+            xvar.setVal(x0_ + 2*dx);
+            double y2p = func.getVal();
+            
+            double x11 = 0.5*(yp - ym), x12 = 0.5*(y2p - y2m);
+            double x21 = ((yp  - ci_[0]) + ( ym - ci_[0]));
+            double x22 = ((y2p - ci_[0]) + (y2m - ci_[0]));
+            ci_[1] = (4*x11/3-x12/6)/dx;
+            ci_[2] = (4*x21/3-x22/12)/(2*dx*dx);
+            ci_[3] = (x12-2*x11)/(6*dx*dx*dx);
+            if (order == 4) {
+                ci_[4] = (x22-4*x21)/(24*dx*dx*dx*dx);
+            }
         }
+        xvar.setVal(x0_);
     }
 }
 
