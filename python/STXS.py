f549cdf69f4a315ca21c26cf80f80ed7bb946d9e
Adinda de Wit <adinda.maite.de.wit@cern.ch>
2021-03-18 17:59:41 +0100

    Add first version of STXS stage1p2 model

--- python/STXS.py
+++ python/STXS.py
@@ -81,3 +81,49 @@ stage1_1_procs = {
   #'other':['tHq','tHW','bbH']
 }
 
+stage1_2_procs = {
+  'ggH':[   'ggH_PTH_200_300',
+            'ggH_PTH_300_450',
+            'ggH_PTH_450_650',
+            'ggH_PTH_GT650',
+            'ggH_0J_PTH_0_10','ggH_0J_PTH_GT10',
+            'ggH_1J_PTH_0_60','ggH_1J_PTH_60_120','ggH_1J_PTH_120_200',
+            'ggH_GE2J_MJJ_0_350_PTH_0_60','ggH_GE2J_MJJ_0_350_PTH_60_120','ggH_GE2J_MJJ_0_350_PTH_120_200',
+            'ggH_GE2J_MJJ_350_700_PTH_0_200_PTHJJ_0_25','ggH_GE2J_MJJ_350_700_PTH_0_200_PTHJJ_GT25',
+            'ggH_GE2J_MJJ_GT700_PTH_0_200_PTHJJ_0_25','ggH_GE2J_MJJ_GT700_PTH_0_200_PTHJJ_GT25'],
+
+  'qqH':[   'qqH_0J','qqH_1J','qqH_GE2J_MJJ_0_60','qqH_GE2J_MJJ_60_120','qqH_GE2J_MJJ_120_350',
+            'qqH_GE2J_MJJ_GT350_PTH_GT200',
+            'qqH_GE2J_MJJ_350_700_PTH_0_200_PTHJJ_0_25','qqH_GE2J_MJJ_350_700_PTH_0_200_PTHJJ_GT25',
+            'qqH_GE2J_MJJ_GT700_PTH_0_200_PTHJJ_0_25','qqH_GE2J_MJJ_GT700_PTH_0_200_PTHJJ_GT25'],
+
+  'WH_lep':['WH_lep_PTV_0_75','WH_lep_PTV_75_150','WH_lep_PTV_150_250_0J','WH_lep_PTV_150_250_GE1J','WH_lep_PTV_GT250'],
+
+  'ZH_lep':['ZH_lep_PTV_0_75','ZH_lep_PTV_75_150','ZH_lep_PTV_150_250_0J','ZH_lep_PTV_150_250_GE1J','ZH_lep_PTV_GT250'],
+
+  'ggZH_lep':['ggZH_lep_PTV_0_75','ggZH_lep_PTV_75_150','ggZH_lep_PTV_150_250_0J','ggZH_lep_PTV_150_250_GE1J','ggZH_lep_PTV_GT250'],
+
+  'VH_had':['WH_had_0J','WH_had_1J','WH_had_GE2J_MJJ_0_60','WH_had_GE2J_MJJ_60_120','WH_had_GE2J_MJJ_120_350',
+            'WH_had_GE2J_MJJ_GT350_PTH_GT200',
+            'WH_had_GE2J_MJJ_350_700_PTH_0_200_PTHJJ_0_25','WH_had_GE2J_MJJ_350_700_PTH_0_200_PTHJJ_GT25',
+            'WH_had_GE2J_MJJ_GT700_PTH_0_200_PTHJJ_0_25','WH_had_GE2J_MJJ_GT700_PTH_0_200_PTHJJ_GT25',
+
+            'ZH_had_0J','ZH_had_1J','ZH_had_GE2J_MJJ_0_60','ZH_had_GE2J_MJJ_60_120','ZH_had_GE2J_MJJ_120_350',
+            'ZH_had_GE2J_MJJ_GT350_PTH_GT200',
+            'ZH_had_GE2J_MJJ_350_700_PTH_0_200_PTHJJ_0_25','ZH_had_GE2J_MJJ_350_700_PTH_0_200_PTHJJ_GT25',
+            'ZH_had_GE2J_MJJ_GT700_PTH_0_200_PTHJJ_0_25','ZH_had_GE2J_MJJ_GT700_PTH_0_200_PTHJJ_GT25'],
+
+
+  'ttH':['ttH_PTH_0_60','ttH_PTH_60_120','ttH_PTH_60_120','ttH_PTH_120_200','ttH_PTH_200_300','ttH_PTH_GT300']#,
+
+}
+
+stage1_2_fine_procs={ #Hopefully temporary while VH(bb) haven't provided correct stage 1.2 splitting
+
+  'WH_lep_PTV_GT250' : ['WH_lep_PTV_250_400','WH_lep_PTV_GT400'],
+  'ZH_lep_PTV_GT250' : ['ZH_lep_PTV_250_400','ZH_lep_PTV_GT400'],
+  'ggZH_lep_PTV_GT250' : ['ggZH_lep_PTV_250_400','ggZH_lep_PTV_GT400']
+
+
+}
+
