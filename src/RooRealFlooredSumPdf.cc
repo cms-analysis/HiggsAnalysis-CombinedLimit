716e6d86c7c7ff68751322ef4ea04960f2919c9d
Jonas Rembser <jonas.rembser@cern.ch>
2021-06-02 17:23:46 +0200

    Replace nameSet1ByIndex with selectFromSet1 in ROOT >= 6.26
    Also nameSet2ByIndex with selectFromSet2. This follows the interface
    change done in ROOT PR https://github.com/root-project/root/pull/7819.

--- src/RooRealFlooredSumPdf.cc
+++ src/RooRealFlooredSumPdf.cc
@@ -325,10 +325,19 @@ Double_t RooRealFlooredSumPdf::analyticalIntegralWN(Int_t code, const RooArgSet*
 	if (cache == 0) { // revive the (sterilized) cache
 		//cout << "RooRealFlooredSumPdf("<<this<<")::analyticalIntegralWN:"<<GetName()<<"("<<code<<","<<(normSet2?*normSet2:RooArgSet())<<","<<(rangeName?rangeName:"<none>") << ": reviving cache "<< endl;
 		std::unique_ptr<RooArgSet> vars(getParameters(RooArgSet()));
+#if ROOT_VERSION_CODE < ROOT_VERSION(6,26,0)
 		std::unique_ptr<RooArgSet> iset(_normIntMgr.nameSet2ByIndex(code - 1)->select(*vars));
 		std::unique_ptr<RooArgSet> nset(_normIntMgr.nameSet1ByIndex(code - 1)->select(*vars));
 		RooArgSet dummy;
 		Int_t code2 = getAnalyticalIntegralWN(*iset, dummy, nset.get(), rangeName);
+#else
+		// In ROOT 6.26, the RooNameSet was removed and the the "selectFromSet*"
+		// functions were introduced to replace its functionality
+		RooArgSet iset{_normIntMgr.selectFromSet2(*vars, code - 1)};
+		RooArgSet nset{_normIntMgr.selectFromSet1(*vars, code - 1)};
+		RooArgSet dummy;
+		Int_t code2 = getAnalyticalIntegralWN(iset, dummy, &nset, rangeName);
+#endif
 		assert(code == code2); // must have revived the right (sterilized) slot...
 		cache = (CacheElem*)_normIntMgr.getObjByIndex(code - 1);
 		assert(cache != 0);
