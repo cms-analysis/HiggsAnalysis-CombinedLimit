40730f4a79d185e50789592a836671d1b0a3fe16
Andrew Gilbert <andrew.gilbert@cern.ch>
2021-08-01 22:37:42 +0200

    Adds SimpleProdPdf class and cleaner VerticalInterpPdf workaround for HZZ
     - For ProdPdfs that factorize (i.e. no common integration observables
       between terms) this class will use less memory than RooProdPdf, as
       we do not make so many intermediate RooRealIntegral objects
     - In the full H->ZZ->4l Run 2 analysis, which uses conditional prod
       pdfs heavily, memory is reduced by around 45%
     - Restored VerticalInterpPdf to its previous behaviour, without HZZ
       specfic hacks. The underlying issue was that RooProdPdf performs its
       own normalization, i.e. it does not guarantee that createIntegral
       returns the appropriate value, since internally the normalization is
       calculated in a different way. For the conditional pdfs we use in
       HZZ, the createIntegral output will always be wrong. Now we
       automatically detect any component conditional RooProdPdf, and
       construct the correct integral as the product of integrals of its own
       components (so this only works correctly if pdf factorizes - NB we do
       not currently check this automatically!)

--- bin/PerfTest.cpp
+++ bin/PerfTest.cpp
@@ -3,14 +3,44 @@
 #include "TROOT.h"
 #include "TEnv.h"
 #include "RooAbsPdf.h"
+#include "RooMsgService.h"
 #include <dlfcn.h>
 #include <RooStats/ModelConfig.h>
 #include "HiggsAnalysis/CombinedLimit/interface/CachingNLL.h"
+#include "HiggsAnalysis/CombinedLimit/interface/ProfilingTools.h"
 
 void (*dump_)(const char *);
 
 
 int main(int argc, char *argv[]) {
+	bool doCombineRuntimes = true;
+	if (doCombineRuntimes) {
+		runtimedef::set("OPTIMIZE_BOUNDS", 1);
+		runtimedef::set("ADDNLL_RECURSIVE", 1);
+		runtimedef::set("ADDNLL_GAUSSNLL", 1);
+		runtimedef::set("ADDNLL_HISTNLL", 1);
+		runtimedef::set("ADDNLL_CBNLL", 1);
+		runtimedef::set("TMCSO_AdaptivePseudoAsimov", 1);
+		// Optimization for bare RooFit likelihoods (--optimizeSimPdf=0)
+		runtimedef::set("MINIMIZER_optimizeConst", 2);
+		runtimedef::set("MINIMIZER_rooFitOffset", 1);
+		// Optimization for ATLAS HistFactory likelihoods
+		runtimedef::set("ADDNLL_ROOREALSUM_FACTOR",1);
+		runtimedef::set("ADDNLL_ROOREALSUM_NONORM",1);
+		runtimedef::set("ADDNLL_ROOREALSUM_BASICINT",1);
+		runtimedef::set("ADDNLL_ROOREALSUM_KEEPZEROS",1);
+		runtimedef::set("ADDNLL_PRODNLL",1);
+		runtimedef::set("ADDNLL_HFNLL",1);
+		runtimedef::set("ADDNLL_HISTFUNCNLL",1);
+		runtimedef::set("ADDNLL_ROOREALSUM_CHEAPPROD",1);
+	}
+	runtimedef::set("ADDNLL_VERBOSE_CACHING", 0);
+	runtimedef::set("fullCloneFunc_VERBOSE", 1);
+	runtimedef::set("CACHINGPDF_NOCLONE", 0); // ! THIS ONE IS NOT SAFE YET
+
+	// Uncomment below for more info
+	// RooMsgService::instance().addStream(RooFit::INFO);
+
 	if (void *sym = dlsym(0, "igprof_dump_now")) {
 	  dump_ = __extension__ (void(*)(const char *)) sym;
 	} else {
@@ -48,7 +78,7 @@ int main(int argc, char *argv[]) {
         const RooCmdArg &constrainCmdArg = RooFit::Constrain(*mc_s->GetNuisanceParameters());
         std::auto_ptr<RooAbsReal> nll;
         nll.reset(pdf.createNLL(*dobs, constrainCmdArg, RooFit::Extended(pdf.canBeExtended()), RooFit::Offset(true))); // make a new nll
-
+        nll->getVal();
 	if(dump_) {
 		dump_("profdump_nll.out.gz");
 	}
