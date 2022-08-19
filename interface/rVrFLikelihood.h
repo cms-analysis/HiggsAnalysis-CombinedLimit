e017776587a08cd20484f677cfde03ea4ab4bc58
Jonas Rembser <jonas.rembser@cern.ch>
2022-08-19 13:49:45 +0200

    Fix invalid proxies in rVrFLikelihood
    The `rVrFLikelihood` has a vector of channels as members, where each
    `Channel` object had a `RooRealProxy` member.
    
    This is not allowed, because when adding channel obejects to the vector,
    memory might be reallocated and the objects are moved. The problem is
    that a `RooRealProxy` should never be moved: its owning `RooAbsArg` has
    a pointer to each proxy, and if the proxies are moved you end up with
    corrupt RooFit objects.
    
    The fix is to use a vector of `unique_ptrs` instead, because like this
    it is only the pointer that is moved and not the proxy object.
    
    It needs to be done at some point because ROOT 6.28 will not allow you
    anymore to make this mistake: the copy/move assigment and construction
    of proxy objects will be explicitely deleted and the `rVrFLikelihood`
    will fail to compile as it is.
    
    This change requires a version bump of the `rVrFLikelihood`, and a
    schema evolution rule to read old versions of this class is also
    implemented.

--- interface/rVrFLikelihood.h
+++ interface/rVrFLikelihood.h
@@ -10,17 +10,20 @@ class rVrFLikelihood : public RooAbsReal {
 
     public:
         rVrFLikelihood() {}
-        rVrFLikelihood(const char *name, const char *title) ;
-        void addChannel(const TH2* chi2, RooAbsReal &muV, RooAbsReal &muF);
-        ~rVrFLikelihood() ;
+        rVrFLikelihood(const char *name, const char *title) : RooAbsReal{name, title} {}
+        rVrFLikelihood(const rVrFLikelihood& other, const char* name=nullptr);
 
-        TObject * clone(const char *newname) const ;
+        void addChannel(const TH2* chi2, RooAbsReal &muV, RooAbsReal &muF);
+
+        TObject* clone(const char* newname) const override {
+            return new rVrFLikelihood(*this,newname);
+        }
 
         // Must be public to get dictionaries to compile properly
-        struct Channel { 
+        struct Channel {
             Channel() {}
             Channel(rVrFLikelihood *parent, const TH2* chi2_, RooAbsReal &muV_, RooAbsReal &muF_) :
-                chi2(chi2_), 
+                chi2(chi2_),
                 muV("muV","signal strength modifier for qqH,VH",  parent, muV_),
                 muF("muF","signal strength modifier for ggH,ttH", parent, muF_) {}
             const TH2* chi2;
@@ -28,12 +31,12 @@ class rVrFLikelihood : public RooAbsReal {
         };
 
     protected:
-        Double_t evaluate() const;
+        Double_t evaluate() const override;
 
     private:
-        std::vector<Channel> channels_;
+        std::vector<std::unique_ptr<Channel>> channels_;
 
-        ClassDef(rVrFLikelihood,1) // Asymmetric power	
+        ClassDefOverride(rVrFLikelihood,2) // Asymmetric power
 };
 
 #endif
