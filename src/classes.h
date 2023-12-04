1c0c812663fbd1328d246123f37c625595112d44
Andrea Malara <andrea.malara@cern.ch>
2023-12-04 21:40:33 +0100

    Create functions for B2G-23-008 analysis

--- src/classes.h
+++ src/classes.h
@@ -66,3 +66,12 @@
 #include "HiggsAnalysis/CombinedLimit/interface/RooCheapProduct.h"
 #include "HiggsAnalysis/CombinedLimit/interface/CMSHggFormula.h"
 #include "HiggsAnalysis/CombinedLimit/interface/SimpleProdPdf.h"
+
+
+#include "HiggsAnalysis/CombinedLimit/interface/PolinomialExponent_1p.hpp"
+#include "HiggsAnalysis/CombinedLimit/interface/PolinomialExponent_2p.hpp"
+#include "HiggsAnalysis/CombinedLimit/interface/PolinomialExponent_3p.hpp"
+#include "HiggsAnalysis/CombinedLimit/interface/PolinomialExponent_4p.hpp"
+#include "HiggsAnalysis/CombinedLimit/interface/PolinomialExponent_5p.hpp"
+#include "HiggsAnalysis/CombinedLimit/interface/PolinomialExponent_6p.hpp"
+#include "HiggsAnalysis/CombinedLimit/interface/ExpGaussExp.hpp"
