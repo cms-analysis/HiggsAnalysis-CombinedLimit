2cf45c30c8a43b2ca818adff7cb6a3e942f23c5a
Aliya Nigamova <aliya.nigamova@cern.ch>
2024-05-23 17:36:31 +0200

    fix compilation

--- src/MultiDimFit.cc
+++ src/MultiDimFit.cc
@@ -743,7 +743,7 @@ void MultiDimFit::doGrid(RooWorkspace *w, RooAbsReal &nll)
             spacingOffsetY = 0.5;
         }
         unsigned int ipoint = 0;
-	// unsigned int nprint = ceil(0.005 * nTotal);
+	      unsigned int nprint = ceil(0.005 * nTotal);
 
         // loop through the grid
         for (unsigned int i = 0; i < nX; ++i) {
