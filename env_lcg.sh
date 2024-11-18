3826032ab0d1161ce5a5cca1b5cbd7e6bebaf661
Jonas Rembser <jonas.rembser@cern.ch>
2024-11-19 00:47:01 +0100

    Update LCG environment script and add full demo on how to use it

--- env_lcg.sh
+++ env_lcg.sh
@@ -1,4 +1,8 @@
-. /cvmfs/sft.cern.ch/lcg/views/LCG_102/x86_64-centos7-gcc11-opt/setup.sh
-export PATH=${PATH}:${PWD}/build/bin
-export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${PWD}/build/lib
-export PYTHONPATH=${PYTHONPATH}:${PWD}/build/lib/python
+LCG_RELEASE=dev4 # or dev3 for ROOT master (dev4 is the latest ROOT stable branch)
+LCG_PATH=/cvmfs/sft.cern.ch/lcg/views/$LCG_RELEASE/latest/x86_64-el9-gcc13-opt
+
+source $LCG_PATH/setup.sh
+source $LCG_PATH/bin/thisroot.sh
+
+export PATH=$PWD/install/bin:$PATH
+export LD_LIBRARY_PATH=$PWD/install/lib:$LD_LIBRARY_PATH
