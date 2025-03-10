2d802b7d2e6955aa1d9977000ebd17d768d480d5
Nick Smith <nick.smith@cern.ch>
2025-03-10 13:45:01 -0500

    Migrate CI to CMake and remove Makefile and any related environments

--- set_conda_env_vars.sh
+++
@@ -1,3 +0,0 @@
-conda env config vars set PATH=${PATH}:${PWD}/build/bin
-conda env config vars set LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${PWD}/build/lib
-conda env config vars set PYTHONPATH=${PYTHONPATH}:${PWD}/build/lib/python
