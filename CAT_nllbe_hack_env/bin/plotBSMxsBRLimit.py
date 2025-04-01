b38e0c159ef24d601e52b248c3a2d306f11543d9
kmoralfi <keila.moral.figueroa@cern.ch>
2025-04-01 14:06:10 +0200

    nllbackend_env

---
+++ CAT_nllbe_hack_env/bin/plotBSMxsBRLimit.py
@@ -0,0 +1,7 @@
+#!/data/dust/user/moralfk/CAT_hackathon/nllbackend_testing/HiggsAnalysis-CombinedLimit/CAT_nllbe_hack_env/bin/python3
+# EASY-INSTALL-DEV-SCRIPT: 'HiggsAnalysisCombinedLimit==1.0','plotBSMxsBRLimit.py'
+__requires__ = 'HiggsAnalysisCombinedLimit==1.0'
+__import__('pkg_resources').require('HiggsAnalysisCombinedLimit==1.0')
+__file__ = '/data/dust/user/moralfk/CAT_hackathon/nllbackend_testing/HiggsAnalysis-CombinedLimit/scripts/plotBSMxsBRLimit.py'
+with open(__file__) as f:
+    exec(compile(f.read(), __file__, 'exec'))
