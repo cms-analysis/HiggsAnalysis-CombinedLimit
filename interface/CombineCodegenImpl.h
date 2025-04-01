b9548aa49b36c1f68a65e23288654af0d4e8fdba
Tom Runting <thomas.runting@cern.ch>
2025-04-01 09:24:22 +0200

    Refactor VerticalInterpPdf to enable codegen backend

--- interface/CombineCodegenImpl.h
+++ interface/CombineCodegenImpl.h
@@ -6,13 +6,22 @@
 #if ROOT_VERSION_CODE >= ROOT_VERSION(6,35,0)
 # define COMBINE_DECLARE_CODEGEN_IMPL(CLASS_NAME) \
     namespace RooFit { namespace Experimental { void codegenImpl(CLASS_NAME &arg, CodegenContext &ctx); }}
+# define COMBINE_DECLARE_CODEGEN_INTEGRAL_IMPL(CLASS_NAME) \
+    namespace RooFit { namespace Experimental { std::string codegenIntegralImpl(CLASS_NAME &arg, int code, const char *rangeName, CodegenContext &ctx); }}
 # define COMBINE_DECLARE_TRANSLATE
+# define COMBINE_DECLARE_ANALYTICAL_INTEGRAL
 #elif ROOT_VERSION_CODE >= ROOT_VERSION(6,32,0)
 # define COMBINE_DECLARE_CODEGEN_IMPL(CLASS_NAME)
-# define COMBINE_DECLARE_TRANSLATE void translate(RooFit::Detail::CodeSquashContext &ctx) const override;
+# define COMBINE_DECLARE_CODEGEN_INTEGRAL_IMPL(CLASS_NAME)
+# define COMBINE_DECLARE_TRANSLATE \
+    void translate(RooFit::Detail::CodeSquashContext &ctx) const override;
+# define COMBINE_DECLARE_ANALYTICAL_INTEGRAL \
+    std::string buildCallToAnalyticIntegral(Int_t code, const char *rangeName, RooFit::Detail::CodeSquashContext &ctx) const override;
 #else
 # define COMBINE_DECLARE_CODEGEN_IMPL(_)
+# define COMBINE_DECLARE_CODEGEN_INTEGRAL_IMPL(_)
 # define COMBINE_DECLARE_TRANSLATE
+# define COMBINE_DECLARE_ANALYTICAL_INTEGRAL
 #endif
 
 #endif
