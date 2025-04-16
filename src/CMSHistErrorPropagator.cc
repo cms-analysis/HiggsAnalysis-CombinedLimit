1a903ca92d7a403d5f1553665d3bef304b53f65f
Aliya Nigamova <aliya.nigamova@cern.ch>
2025-04-16 16:36:46 +0200

    add x_ to allVars manually to enable analytic integration in CMSHistErrorPropagator with ROOT 6.32

--- src/CMSHistErrorPropagator.cc
+++ src/CMSHistErrorPropagator.cc
@@ -506,6 +506,9 @@ void CMSHistErrorPropagator::printMultiline(std::ostream& os, Int_t contents, Bo
 Int_t CMSHistErrorPropagator::getAnalyticalIntegral(RooArgSet& allVars,
                                          RooArgSet& analVars,
                                          const char* /*rangeName*/) const {
+  #if ROOT_VERSION_CODE >= ROOT_VERSION(6,32,0) // force x_ as CMSHistErrorPropagator dependent (needed since ROOT 6.32 to support analytic integration)
+  allVars.add(x_.arg());
+  #endif
   if (matchArgs(allVars, analVars, x_)) return 1;
   return 0;
 }
