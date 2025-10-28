192c3558e9a1f19131e181b1907460f12c10ab2f
Massimiliano Galli <massimiliano.galli@cern.ch>
2025-10-28 10:56:13 +0100

    First version of test to create and minimize a likelihood without using command line

--- env_lcg.sh
+++ env_lcg.sh
@@ -1,4 +1,4 @@
-LCG_RELEASE=dev4 # or dev3 for ROOT master (dev4 is the latest ROOT stable branch)
+LCG_RELEASE=dev3 # or dev3 for ROOT master (dev4 is the latest ROOT stable branch)
 LCG_PATH=/cvmfs/sft.cern.ch/lcg/views/$LCG_RELEASE/latest/x86_64-el9-gcc13-opt
 
 source $LCG_PATH/setup.sh
