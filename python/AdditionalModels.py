cms-analysis/HiggsAnalysis-CombinedLimit

commit dc9380fe7bc731016f36908c461bae160531136c

Avoid equality operator to replace with `None`
This is bad practice in Python, because it only works if the type of the
operand implements the equality operator.

In particular, it will fail with newer ROOT versions, because the
`TObject` base class won't implement the equality operator anymore:
https://github.com/root-project/root/pull/19145

This commit was done automatically with code modernization tools.

python/AdditionalModels.py
@@ -107,7 +107,7 @@ class C8(SMLikeHiggsModel):
         name = f"c8_XSBRscal_{production}_{decay}"
         print("[LOFullParametrization::C7]")
         print(name, production, decay, energy)
-        if self.modelBuilder.out.function(name) == None:
+        if self.modelBuilder.out.function(name) is None:
             XSscal = "kgluon"
             if production in ["WH", "ZH", "VH", "qqH"]:
                 XSscal = "kV"
@@ -266,7 +266,7 @@ class CWidth(SMLikeHiggsModel):
         name = f"c7_XSBRscal_{production}_{decay}"
         print("[LOFullParametrization::C7]")
         print(name, production, decay, energy)
-        if self.modelBuilder.out.function(name) == None:
+        if self.modelBuilder.out.function(name) is None:
             XSscal = "kgluon"
             if production in ["WH", "ZH", "VH", "qqH"]:
                 XSscal = "kV"

python/HiggsBenchmarkModels/CustodialSymmetryModels.py
@@ -318,7 +318,7 @@ class CzwHiggs(SMLikeHiggsModel):
             return 0
 
         name = f"Czw_XSBRscal_{production}_{decay}_{energy}"
-        if self.modelBuilder.out.function(name) == None:
+        if self.modelBuilder.out.function(name) is None:
             if production in ["ggH", "ttH"]:
                 self.modelBuilder.factory_(f'expr::{name}("@0", Czw_BRscal_{decay})')
             else:
@@ -399,7 +399,7 @@ class CwzHiggs(SMLikeHiggsModel):
             return 0
 
         name = f"Cwz_XSBRscal_{production}_{decay}_{energy}"
-        if self.modelBuilder.out.function(name) == None:
+        if self.modelBuilder.out.function(name) is None:
             if production in ["ggH", "ttH"]:
                 self.modelBuilder.factory_(f'expr::{name}("@0", Cwz_BRscal_{decay})')
             else:

python/HiggsBenchmarkModels/FermionSectorModels.py
@@ -353,7 +353,7 @@ class C5qlHiggs(SMLikeHiggsModel):
 
     def getHiggsSignalYieldScale(self, production, decay, energy):
         name = f"C5ql_XSBRscal_{production}_{decay}"
-        if self.modelBuilder.out.function(name) == None:
+        if self.modelBuilder.out.function(name) is None:
             XSscal = "Cglu" if production in ["ggH"] else "Cv"
             if production in ["ttH"]:
                 XSscal = "Cf" if self.universalCF else "Cq"
@@ -470,7 +470,7 @@ class C5udHiggs(SMLikeHiggsModel):
 
     def getHiggsSignalYieldScale(self, production, decay, energy):
         name = f"C5ud_XSBRscal_{production}_{decay}"
-        if self.modelBuilder.out.function(name) == None:
+        if self.modelBuilder.out.function(name) is None:
             XSscal = "Cglu" if production in ["ggH"] else "Cv"
             if production in ["ttH"]:
                 XSscal = "Cf" if self.universalCF else "Cu"

python/HiggsBenchmarkModels/VectorsAndFermionsModels.py
@@ -216,7 +216,7 @@ class CvCfXgHiggs(SMLikeHiggsModel):
 
     def getHiggsSignalYieldScale(self, production, decay, energy):
         name = f"CvCfXg_XSBRscal_{production}_{decay}"
-        if self.modelBuilder.out.function(name) == None:
+        if self.modelBuilder.out.function(name) is None:
             XSscal = "CF" if production in ["ggH", "ttH"] else "CV"
             BRscal = "hgg"
             if decay in ["hww", "hzz"]:
@@ -313,7 +313,7 @@ class CfXgHiggs(SMLikeHiggsModel):
 
     def getHiggsSignalYieldScale(self, production, decay, energy):
         name = f"CfXg_XSBRscal_{production}_{decay}"
-        if self.modelBuilder.out.function(name) == None:
+        if self.modelBuilder.out.function(name) is None:
             XSscal = "CF" if production in ["ggH", "ttH"] else "CV"
             BRscal = "hgg"
             if decay in ["hww", "hzz"]:

python/HiggsCouplingsLOSM.py
@@ -89,7 +89,7 @@ class CvCfHiggsLOSM(SMLikeHiggsModel):
 
     def getHiggsSignalYieldScale(self, production, decay, energy):
         name = f"CvCf_XSBRscal_{production}_{decay}"
-        if self.modelBuilder.out.function(name) == None:
+        if self.modelBuilder.out.function(name) is None:
             XSscal = "CF" if production in ["ggH", "ttH"] else "CV"
             BRscal = "hgg"
             if decay in ["hww", "hzz"]:
@@ -186,7 +186,7 @@ class CvCfXgHiggsLOSM(SMLikeHiggsModel):
 
     def getHiggsSignalYieldScale(self, production, decay, energy):
         name = f"CvCfXg_XSBRscal_{production}_{decay}"
-        if self.modelBuilder.out.function(name) == None:
+        if self.modelBuilder.out.function(name) is None:
             XSscal = "CF" if production in ["ggH", "ttH"] else "CV"
             BRscal = "hgg"
             if decay in ["hww", "hzz"]:
@@ -283,7 +283,7 @@ class CfXgHiggsLOSM(SMLikeHiggsModel):
 
     def getHiggsSignalYieldScale(self, production, decay, energy):
         name = f"CfXg_XSBRscal_{production}_{decay}"
-        if self.modelBuilder.out.function(name) == None:
+        if self.modelBuilder.out.function(name) is None:
             XSscal = "CF" if production in ["ggH", "ttH"] else "CV"
             BRscal = "hgg"
             if decay in ["hww", "hzz"]:

commit 7de3ed81c64a1719b65abffee61124092eff8fbd

Replace nullptr comparisons with `None` with bool checks
Exploiting comparisons with `None` as nullptr checks is quite
dangerous, because even though it used to work to do checks like
`x == None` when x was a `nullptr`, this is not idiomatic Python.

The `None` object is of a singleton type with only one possible type
(`None` of type `NoneType`), and all comparisons should fail is the
value is not exactly `None`. That's why linters suggest to use
`x is None` instead of `x == None`. This is however breaking the nullptr
comparison (see https://github.com/root-project/root/issues/20283).

The safe way to do `nullptr` comparison is to either do
`x == ROOT.nullptr`, or to rely on the implicit conversion of pointers
to `bool`. This is what is implemented in this PR, fixing the wrong
behavior introduced by (dc9380fe and 824c6e41).

python/AdditionalModels.py
@@ -107,7 +107,7 @@ class C8(SMLikeHiggsModel):
         name = f"c8_XSBRscal_{production}_{decay}"
         print("[LOFullParametrization::C7]")
         print(name, production, decay, energy)
-        if self.modelBuilder.out.function(name) == None:
+        if not self.modelBuilder.out.function(name):
             XSscal = "kgluon"
             if production in ["WH", "ZH", "VH", "qqH"]:
                 XSscal = "kV"
@@ -266,7 +266,7 @@ class CWidth(SMLikeHiggsModel):
         name = f"c7_XSBRscal_{production}_{decay}"
         print("[LOFullParametrization::C7]")
         print(name, production, decay, energy)
-        if self.modelBuilder.out.function(name) == None:
+        if not self.modelBuilder.out.function(name):
             XSscal = "kgluon"
             if production in ["WH", "ZH", "VH", "qqH"]:
                 XSscal = "kV"

python/HiggsBenchmarkModels/CustodialSymmetryModels.py
@@ -318,7 +318,7 @@ class CzwHiggs(SMLikeHiggsModel):
             return 0
 
         name = f"Czw_XSBRscal_{production}_{decay}_{energy}"
-        if self.modelBuilder.out.function(name) == None:
+        if not self.modelBuilder.out.function(name):
             if production in ["ggH", "ttH"]:
                 self.modelBuilder.factory_(f'expr::{name}("@0", Czw_BRscal_{decay})')
             else:
@@ -399,7 +399,7 @@ class CwzHiggs(SMLikeHiggsModel):
             return 0
 
         name = f"Cwz_XSBRscal_{production}_{decay}_{energy}"
-        if self.modelBuilder.out.function(name) == None:
+        if not self.modelBuilder.out.function(name):
             if production in ["ggH", "ttH"]:
                 self.modelBuilder.factory_(f'expr::{name}("@0", Cwz_BRscal_{decay})')
             else:

python/HiggsBenchmarkModels/FermionSectorModels.py
@@ -353,7 +353,7 @@ class C5qlHiggs(SMLikeHiggsModel):
 
     def getHiggsSignalYieldScale(self, production, decay, energy):
         name = f"C5ql_XSBRscal_{production}_{decay}"
-        if self.modelBuilder.out.function(name) == None:
+        if not self.modelBuilder.out.function(name):
             XSscal = "Cglu" if production in ["ggH"] else "Cv"
             if production in ["ttH"]:
                 XSscal = "Cf" if self.universalCF else "Cq"
@@ -470,7 +470,7 @@ class C5udHiggs(SMLikeHiggsModel):
 
     def getHiggsSignalYieldScale(self, production, decay, energy):
         name = f"C5ud_XSBRscal_{production}_{decay}"
-        if self.modelBuilder.out.function(name) == None:
+        if not self.modelBuilder.out.function(name):
             XSscal = "Cglu" if production in ["ggH"] else "Cv"
             if production in ["ttH"]:
                 XSscal = "Cf" if self.universalCF else "Cu"

python/HiggsBenchmarkModels/VectorsAndFermionsModels.py
@@ -216,7 +216,7 @@ class CvCfXgHiggs(SMLikeHiggsModel):
 
     def getHiggsSignalYieldScale(self, production, decay, energy):
         name = f"CvCfXg_XSBRscal_{production}_{decay}"
-        if self.modelBuilder.out.function(name) == None:
+        if not self.modelBuilder.out.function(name):
             XSscal = "CF" if production in ["ggH", "ttH"] else "CV"
             BRscal = "hgg"
             if decay in ["hww", "hzz"]:
@@ -313,7 +313,7 @@ class CfXgHiggs(SMLikeHiggsModel):
 
     def getHiggsSignalYieldScale(self, production, decay, energy):
         name = f"CfXg_XSBRscal_{production}_{decay}"
-        if self.modelBuilder.out.function(name) == None:
+        if not self.modelBuilder.out.function(name):
             XSscal = "CF" if production in ["ggH", "ttH"] else "CV"
             BRscal = "hgg"
             if decay in ["hww", "hzz"]:

python/HiggsCouplingsLOSM.py
@@ -89,7 +89,7 @@ class CvCfHiggsLOSM(SMLikeHiggsModel):
 
     def getHiggsSignalYieldScale(self, production, decay, energy):
         name = f"CvCf_XSBRscal_{production}_{decay}"
-        if self.modelBuilder.out.function(name) == None:
+        if not self.modelBuilder.out.function(name):
             XSscal = "CF" if production in ["ggH", "ttH"] else "CV"
             BRscal = "hgg"
             if decay in ["hww", "hzz"]:
@@ -186,7 +186,7 @@ class CvCfXgHiggsLOSM(SMLikeHiggsModel):
 
     def getHiggsSignalYieldScale(self, production, decay, energy):
         name = f"CvCfXg_XSBRscal_{production}_{decay}"
-        if self.modelBuilder.out.function(name) == None:
+        if not self.modelBuilder.out.function(name):
             XSscal = "CF" if production in ["ggH", "ttH"] else "CV"
             BRscal = "hgg"
             if decay in ["hww", "hzz"]:
@@ -283,7 +283,7 @@ class CfXgHiggsLOSM(SMLikeHiggsModel):
 
     def getHiggsSignalYieldScale(self, production, decay, energy):
         name = f"CfXg_XSBRscal_{production}_{decay}"
-        if self.modelBuilder.out.function(name) == None:
+        if not self.modelBuilder.out.function(name):
             XSscal = "CF" if production in ["ggH", "ttH"] else "CV"
             BRscal = "hgg"
             if decay in ["hww", "hzz"]:

python/HiggsCouplings_ICHEP12.py
@@ -93,7 +93,7 @@ class CvCfHiggs(SMLikeHiggsModel):
 
     def getHiggsSignalYieldScale(self, production, decay, energy):
         name = f"CvCf_XSBRscal_{production}_{decay}"
-        if self.modelBuilder.out.function(name) == None:
+        if not self.modelBuilder.out.function(name):
             XSscal = "CF" if production in ["ggH", "ttH"] else "CV"
             BRscal = "hgg"
             if decay in ["hww", "hzz"]:
@@ -208,7 +208,7 @@ class C5qlHiggs(SMLikeHiggsModel):
 
     def getHiggsSignalYieldScale(self, production, decay, energy):
         name = f"C5ql_XSBRscal_{production}_{decay}"
-        if self.modelBuilder.out.function(name) == None:
+        if not self.modelBuilder.out.function(name):
             XSscal = "Cglu" if production in ["ggH"] else "Cv"
             if production in ["ttH"]:
                 XSscal = "Cf" if self.universalCF else "Cq"
@@ -325,7 +325,7 @@ class C5udHiggs(SMLikeHiggsModel):
 
     def getHiggsSignalYieldScale(self, production, decay, energy):
         name = f"C5ud_XSBRscal_{production}_{decay}"
-        if self.modelBuilder.out.function(name) == None:
+        if not self.modelBuilder.out.function(name):
             XSscal = "Cglu" if production in ["ggH"] else "Cv"
             if production in ["ttH"]:
                 XSscal = "Cf" if self.universalCF else "Cu"
@@ -516,7 +516,7 @@ class CzwHiggs(SMLikeHiggsModel):
             return 0
 
         name = f"Czw_XSBRscal_{production}_{decay}_{energy}"
-        if self.modelBuilder.out.function(name) == None:
+        if not self.modelBuilder.out.function(name):
             if production in ["ggH", "ttH"]:
                 self.modelBuilder.factory_(f'expr::{name}("@0", Czw_BRscal_{decay})')
             else:
@@ -598,7 +598,7 @@ class CwzHiggs(SMLikeHiggsModel):
             return 0
 
         name = f"Cwz_XSBRscal_{production}_{decay}_{energy}"
-        if self.modelBuilder.out.function(name) == None:
+        if not self.modelBuilder.out.function(name):
             if production in ["ggH", "ttH"]:
                 self.modelBuilder.factory_(f'expr::{name}("@0", Cwz_BRscal_{decay})')
             else:

python/InvisibleWidth.py
@@ -87,7 +87,7 @@ class InvisibleWidth(SMLikeHiggsModel):
     def getHiggsSignalYieldScale(self, production, decay, energy):
         name = f"invisibleWidth_XSBRscal_{production}_{decay}"
         print(name, production, decay, energy)
-        if self.modelBuilder.out.function(name) == None:
+        if not self.modelBuilder.out.function(name):
             XSscal = "kgluon"
             if production in ["WH", "ZH", "VH", "qqH"]:
                 XSscal = "kV"

python/LHCHCGModels.py
@@ -573,7 +573,7 @@ class Kappas(LHCHCGBaseModel):
 
     def getHiggsSignalYieldScale(self, production, decay, energy):
         name = f"c7_XSBRscal_{production}_{decay}_{energy}"
-        if self.modelBuilder.out.function(name) == None:
+        if not self.modelBuilder.out.function(name):
             if production in ["ggH", "qqH", "ggZH", "tHq", "tHW"]:
                 XSscal = ("@0", f"Scaling_{production}_{energy}")
             elif production == "WH":
@@ -848,7 +848,7 @@ class KappaVKappaF(LHCHCGBaseModel):
 
     def getHiggsSignalYieldScale(self, production, decay, energy):
         name = f"c7_XSBRscal_{production}_{decay}_{energy}"
-        if self.modelBuilder.out.function(name) == None:
+        if not self.modelBuilder.out.function(name):
             if production in ["ggZH", "tHq", "tHW"]:
                 XSscal = (
                     "@0",

python/LOFullParametrization.py
@@ -82,7 +82,7 @@ class C5(SMLikeHiggsModel):
         name = f"c6_XSBRscal_{production}_{decay}"
         print("[LOFullParametrization::C6]")
         print(name, production, decay, energy)
-        if self.modelBuilder.out.function(name) == None:
+        if not self.modelBuilder.out.function(name):
             XSscal = "kgluon"
             if production in ["WH", "ZH", "VH", "qqH"]:
                 XSscal = "kV"
@@ -191,7 +191,7 @@ class C6(SMLikeHiggsModel):
         name = f"c6_XSBRscal_{production}_{decay}"
         print("[LOFullParametrization::C6]")
         print(name, production, decay, energy)
-        if self.modelBuilder.out.function(name) == None:
+        if not self.modelBuilder.out.function(name):
             XSscal = "kgluon"
             if production in ["WH", "ZH", "VH", "qqH"]:
                 XSscal = "kV"
@@ -314,7 +314,7 @@ class C7(SMLikeHiggsModel):
         name = f"c7_XSBRscal_{production}_{decay}"
         print("[LOFullParametrization::C7]")
         print(name, production, decay, energy)
-        if self.modelBuilder.out.function(name) == None:
+        if not self.modelBuilder.out.function(name):
             XSscal = "kgluon"
             if production in ["WH", "ZH", "VH", "qqH"]:
                 XSscal = "kV"

python/SMHiggsBuilder.py
@@ -303,7 +303,7 @@ class SMHiggsBuilder:
                         if D in DS:
                             var = self.modelBuilder.out.var("HiggsDecayWidthTHU_%s" % K2)
                             break
-                    if var == None:
+                    if not var:
                         continue
                 else:
                     var = self.modelBuilder.out.var("param_%s" % K)
@@ -315,7 +315,7 @@ class SMHiggsBuilder:
     def dump(self, name, xvar, values, logfile):
         xv = self.modelBuilder.out.var(xvar)
         yf = self.modelBuilder.out.function(name)
-        if yf == None:
+        if not yf:
             raise RuntimeError("Missing " + name)
         log = open(logfile, "w")
         for x in values:
@@ -323,7 +323,7 @@ class SMHiggsBuilder:
             log.write(f"{x:.3f}\t{yf.getVal():.7g}\n")
 
     def textToSpline(self, name, filename, xvar="MH", ycol=1, xcol=0, skipRows=1, algo="CSPLINE"):
-        if self.modelBuilder.out.function(name) != None:
+        if self.modelBuilder.out.function(name):
             return
         x = []
         y = []

python/STXStoEFTModel.py
@@ -516,7 +516,7 @@ class AllStagesToEFTModel(STXStoEFTBaseModel):
         production = convert_to_STXS(production, decay)
 
         name = f"stxstoeft_scaling_{production}_{decay}_{energy}"
-        if self.modelBuilder.out.function(name) == None:
+        if not self.modelBuilder.out.function(name):
             XSscal = None
             BRscal = None
 
@@ -663,7 +663,7 @@ class StageXToEFTModel(STXStoEFTBaseModel):
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     def getHiggsSignalYieldScale(self, production, decay, energy):
         name = f"stxstoeft_scaling_{production}_{decay}_{energy}"
-        if self.modelBuilder.out.function(name) == None:
+        if not self.modelBuilder.out.function(name):
             XSscal = None
             BRscal = None
 

python/STXStoSMEFTModel.py
@@ -589,13 +589,13 @@ class STXSToSMEFTModel(STXStoSMEFTBaseModel):
         if self.expand_equations:
             if self.linear_only:
                 name = f"scaling_linear_expand_XS_{production}_BR_{decay}"
-                if self.modelBuilder.out.function(name) == None:
+                if not self.modelBuilder.out.function(name):
                     print(f" --> [STXStoSMEFT] Making linearised model for (STXS bin,decay): ({production},{decay})")
                     self.makeScalingFunction_expand(production, decay)
 
             elif self.linquad_only:
                 name = f"scaling_linquad_expand_XS_{production}_BR_{decay}"
-                if self.modelBuilder.out.function(name) == None:
+                if not self.modelBuilder.out.function(name):
                     print(f" --> [STXStoSMEFT] Making linear+quad model for (STXS bin,decay): ({production},{decay})")
                     self.makeScalingFunction_expand(production, decay)
 
@@ -603,7 +603,7 @@ class STXSToSMEFTModel(STXStoSMEFTBaseModel):
                 name = f"stxstosmeft_scaling_expand_XS_{production}_BR_{decay}"
                 name_lin = f"scaling_linear_expand_XS_{production}_BR_{decay}"
                 name_linquad = f"scaling_linquad_expand_XS_{production}_BR_{decay}"
-                if (self.modelBuilder.out.function(name_lin) == None) | (self.modelBuilder.out.function(name_linquad) == None):
+                if (not self.modelBuilder.out.function(name_lin)) | (not self.modelBuilder.out.function(name_linquad)):
                     print(f" --> [STXStoSMEFT] Making linearised and linear+quadratic model for (STXS bin,decay): ({production},{decay})")
                     self.makeScalingFunction_expand(production, decay)
                 # Combine linear and linear+quadratic models into same scaling (use different POIs)
@@ -613,65 +613,65 @@ class STXSToSMEFTModel(STXStoSMEFTBaseModel):
         else:
             if self.linear_only:
                 name_lin_xs = "scaling_linear_XS_%s" % production
-                if self.modelBuilder.out.function(name_lin_xs) == None:
+                if not self.modelBuilder.out.function(name_lin_xs):
                     print(" --> [STXStoSMEFT] Making linear scaling function for STXS bin: %s" % production)
                     self.makeScalingFunction(production, mode="linear")
                 name_lin_br = "scaling_linear_BR_%s" % decay
-                if self.modelBuilder.out.function(name_lin_br) == None:
+                if not self.modelBuilder.out.function(name_lin_br):
                     print(" --> [STXStoSMEFT] Making linear scaling function for decay: %s" % decay)
                     self.makeScalingFunction(decay, is_decay=True, mode="linear")
                     self.makeBRScalingFunction(decay, mode="linear")
                 name = f"scaling_linear_XS_{production}_BR_{decay}"
-                if self.modelBuilder.out.function(name) == None:
+                if not self.modelBuilder.out.function(name):
                     self.modelBuilder.factory_("prod::{}({})".format(name, ",".join([name_lin_xs, name_lin_br])))
 
             elif self.linquad_only:
                 name_linquad_xs = "scaling_linquad_XS_%s" % production
-                if self.modelBuilder.out.function(name_linquad_xs) == None:
+                if not self.modelBuilder.out.function(name_linquad_xs):
                     print(" --> [STXStoSMEFT] Making linear+quad scaling function for STXS bin: %s" % production)
                     self.makeScalingFunction(production, mode="linquad")
                 name_linquad_br = "scaling_linquad_BR_%s" % decay
-                if self.modelBuilder.out.function(name_linquad_br) == None:
+                if not self.modelBuilder.out.function(name_linquad_br):
                     print(" --> [STXStoSMEFT] Making linear+quad scaling function for decay: %s" % decay)
                     self.makeScalingFunction(decay, is_decay=True, mode="linquad")
                     self.makeBRScalingFunction(decay, mode="linquad")
                 name = f"scaling_linquad_XS_{production}_BR_{decay}"
-                if self.modelBuilder.out.function(name) == None:
+                if not self.modelBuilder.out.function(name):
                     self.modelBuilder.factory_("prod::{}({})".format(name, ",".join([name_linquad_xs, name_linquad_br])))
 
             # Combine linear and linear+quadratic models into same scaling (use different POIs) as product
             else:
                 # Linear part
                 name_lin_xs = "scaling_linear_XS_%s" % production
-                if self.modelBuilder.out.function(name_lin_xs) == None:
+                if not self.modelBuilder.out.function(name_lin_xs):
                     print(" --> [STXStoSMEFT] Making linear scaling function for STXS bin: %s" % production)
                     self.makeScalingFunction(production, mode="linear")
                 name_lin_br = "scaling_linear_BR_%s" % decay
-                if self.modelBuilder.out.function(name_lin_br) == None:
+                if not self.modelBuilder.out.function(name_lin_br):
                     print(" --> [STXStoSMEFT] Making linear scaling function for decay: %s" % decay)
                     self.makeScalingFunction(decay, is_decay=True, mode="linear")
                     self.makeBRScalingFunction(decay, mode="linear")
                 name_lin = f"scaling_linear_XS_{production}_BR_{decay}"
-                if self.modelBuilder.out.function(name_lin) == None:
+                if not self.modelBuilder.out.function(name_lin):
                     self.modelBuilder.factory_("prod::{}({})".format(name_lin, ",".join([name_lin_xs, name_lin_br])))
 
                 # Linear+quadratic part
                 name_linquad_xs = "scaling_linquad_XS_%s" % production
-                if self.modelBuilder.out.function(name_linquad_xs) == None:
+                if not self.modelBuilder.out.function(name_linquad_xs):
                     print(" --> [STXStoSMEFT] Making linear+quad scaling function for STXS bin: %s" % production)
                     self.makeScalingFunction(production, mode="linquad")
                 name_linquad_br = "scaling_linquad_BR_%s" % decay
-                if self.modelBuilder.out.function(name_linquad_br) == None:
+                if not self.modelBuilder.out.function(name_linquad_br):
                     print(" --> [STXStoSMEFT] Making linear+quad scaling function for decay: %s" % decay)
                     self.makeScalingFunction(decay, is_decay=True, mode="linquad")
                     self.makeBRScalingFunction(decay, mode="linquad")
                 name_linquad = f"scaling_linquad_XS_{production}_BR_{decay}"
-                if self.modelBuilder.out.function(name_linquad) == None:
+                if not self.modelBuilder.out.function(name_linquad):
                     self.modelBuilder.factory_("prod::{}({})".format(name_linquad, ",".join([name_linquad_xs, name_linquad_br])))
 
                 # Combination
                 name = f"stxstosmeft_scaling_XS_{production}_BR_{decay}"
-                if self.modelBuilder.out.function(name) == None:
+                if not self.modelBuilder.out.function(name):
                     self.modelBuilder.factory_("prod::{}({})".format(name, ",".join([name_lin, name_linquad])))
 
         return name

python/ShapeTools.py
@@ -474,7 +474,7 @@ class ShapeBuilder(ModelBuilder):
                     continue
                 shape = self.getShape(b, p)
                 norm = 0
-                if shape == None:  # counting experiment
+                if not shape:  # counting experiment
                     if not self.out.var("CMS_fakeObs"):
                         self.doVar("CMS_fakeObs[0,1]")
                         self.out.var("CMS_fakeObs").setBins(1)
@@ -849,8 +849,8 @@ class ShapeBuilder(ModelBuilder):
         if (channel, process) in _cache:
             return _cache[(channel, process)]
         shapeNominal = self.getShape(channel, process)
-        nominalPdf = self.shape2Pdf(shapeNominal, channel, process) if (self.options.useHistPdf == "always" or shapeNominal == None) else shapeNominal
-        if shapeNominal == None:
+        nominalPdf = self.shape2Pdf(shapeNominal, channel, process) if (self.options.useHistPdf == "always" or not shapeNominal) else shapeNominal
+        if not shapeNominal:
             return nominalPdf  # no point morphing a fake shape
         morphs = []
         shapeAlgo = None
@@ -1098,7 +1098,7 @@ class ShapeBuilder(ModelBuilder):
         postFix = "Sig" if (process in self.DC.isSignal and self.DC.isSignal[process]) else "Bkg"
         terms = []
         shapeNominal = self.getShape(channel, process)
-        if shapeNominal == None:
+        if not shapeNominal:
             # FIXME no extra norm for dummy pdfs (could be changed)
             return None
         if shapeNominal.InheritsFrom("RooAbsPdf") and not shapeNominal.InheritsFrom("RooParametricHist") or shapeNominal.InheritsFrom("CMSHistFunc"):
@@ -1204,7 +1204,7 @@ class ShapeBuilder(ModelBuilder):
 
     def shape2Data(self, shape, channel, process, _cache={}):
         postFix = "Sig" if (process in self.DC.isSignal and self.DC.isSignal[process]) else "Bkg"
-        if shape == None:
+        if not shape:
             name = f"shape{postFix}_{channel}_{process}"
             if name not in _cache:
                 obs = ROOT.RooArgSet(self.out.var("CMS_fakeObs"))

python/SingleTopModels.py
@@ -145,7 +145,7 @@ class KappaVKappaT(LHCHCGBaseModel):
 
     def getHiggsSignalYieldScale(self, production, decay, energy):
         name = f"c7_XSBRscal_{production}_{decay}_{energy}"
-        if self.modelBuilder.out.function(name) == None:
+        if not self.modelBuilder.out.function(name):
             if production in ["ggH", "qqH", "ggZH", "tHq", "tHW"]:
                 XSscal = ("@0", f"Scaling_{production}_{energy}")
             elif production == "WH":

python/TrilinearCouplingModels.py
@@ -142,7 +142,7 @@ class TrilinearHiggsKappaVKappaF(LHCHCGBaseModel):
 
     def getHiggsSignalYieldScale(self, production, decay, energy):
         name = f"kVkFkl_XSBRscal_{production}_{decay}_{energy}"
-        if self.modelBuilder.out.function(name) == None:
+        if not self.modelBuilder.out.function(name):
             # now make production scaling --> taken from Tab. 2 of https://arxiv.org/pdf/1607.04251v1.pdf, using formula from https://arxiv.org/pdf/1709.08649.pdf (eqn 18)
             cXSmap_7 = {
                 "ggH": 0.66e-2,
@@ -381,7 +381,7 @@ class TrilinearHiggsDifferential(PhysicsModel):
         name = f"XSBRscal_{production}_{decay}"
         print(name)
         # If name has been defined in doParameterOfInterest()
-        if self.modelBuilder.out.function(name) == None:
+        if not self.modelBuilder.out.function(name):
             print("DEBUG: proc x genbin signal has not been given a scale factor")
             # return 0
         # else:
@@ -647,7 +647,7 @@ class TrilinearHiggsKappaVKappaFSTXS12(LHCHCGBaseModel):
             raise RuntimeError("Decay mode %s not supported" % decay)
 
         XSBRscaling = f"{XSscal}_{BRscal}"
-        if self.modelBuilder.out.function(XSBRscaling) == None:
+        if not self.modelBuilder.out.function(XSBRscaling):
             self.modelBuilder.factory_(f'expr::{XSBRscaling}("@0*@1", {XSscal}, {BRscal})')
             print()
             self.modelBuilder.out.function(XSBRscaling).Print("")