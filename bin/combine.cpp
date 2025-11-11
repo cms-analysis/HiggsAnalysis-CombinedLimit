ac6d3f5c2e85109b5fa5071be403d431461e9b8f
Massimiliano Galli <massimiliano.galli@cern.ch>
2025-11-11 11:33:14 +0100

    Update version to v10.3.2

--- bin/combine.cpp
+++ bin/combine.cpp
@@ -33,7 +33,7 @@
 using namespace std;
 
 // Update whenever we have a new Tag
-std::string combineTagString = "v10.3.1";
+std::string combineTagString = "v10.3.2";
 // 
 
 int main(int argc, char **argv) {
